23de330dc134281a581ebe9da833253ff251ed7a
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-05-12 21:39:16 +0100

    Modifications to Orbits.m and NonstandardBCs.m

--- ode/NonstandardBCs.m
+++ ode/NonstandardBCs.m
@@ -7,7 +7,7 @@
 
 %%
 % With the advances of automatic differentiation of scalars in Chebfun it
-% is now possible to solve ODEs in Chebfun with more exotic constraints
+% became possible to solve ODEs in Chebfun with more exotic constraints
 % than simply conditions at the endpoints of the domain. These include
 % interior point conditions, as well as conditions on the function over the
 % whole domain.
@@ -18,14 +18,13 @@
 %% Example 1: Mean zero solution
 % In this example, we solve the ODE
 %
-%     u'' + x.^2*u = 1
+% $$ u" + x^2 u = 1 $$
 %
-% on the domain [-1,1] such that
+% on the domain $[-1,1]$ subject to the conditions
 %
-%     u(-1) = 1;
-%     The average of u over [-1,1] is 0
+% $$ u(-1) = 1; \quad \mbox{The average of $u$ over $[-1,1]$ is $0$} $$
 %
-% Equivalent way to state the second condition is that the definite
+% An equivalent way to state the second condition is that the definite
 % integral of the solution over the domain must be zero.
 
 %%
@@ -39,7 +38,7 @@ N.lbc = 1;
 
 %%
 % However, the second condition applies on the function over the whole
-% domain. We use the field .bc to enforce that condition
+% domain. We use the field .bc to enforce the condition
 N.bc = @(x,u) sum(u);
 
 %%
@@ -54,75 +53,78 @@ N.bc = @(x,u) sum(u);
 %%
 % We now solve the problem in the regular way using backslash and plot the
 % solution:
-u = N\1; plot(u)
+u = N\1; plot(u,LW,1.6), grid on
 %%
 % We confirm that we've solved the problem by calculating the residual of
 % the differential equation and both the conditions:
-disp(['Residual of Differ. equation:   ', num2str(norm(N(u)-1))])
-disp(['Residual of Left BC:            ', num2str(abs(u(-1)-1))])
-disp(['Residual of Interior Condition: ', num2str(abs(sum(u)))])
+disp(['Residual of differential equation: ', num2str(norm(N(u)-1))])
+disp(['Residual of left BC:               ', num2str(abs(u(-1)-1))])
+disp(['Residual of interior condition:    ', num2str(abs(sum(u)))])
 %%
-% We could also require the mean of u to take another value,
-% e.g. 1, via
+% We could also require the mean of $u$ to take another value,
+% e.g. $1$, via
 N.bc = @(x,u) mean(u)-1;
 
 %%
 % If we now solve the problem, we see that this condition is satisfied to a
 % high accuracy
 u = N\1;
-disp(['Residual of Interior Condition: ', num2str(abs(mean(u)-1))])
+disp(['Residual of Interior condition: ', num2str(abs(mean(u)-1))])
 %%
 % Finally, we could also introduce some weighting, for example, we could
-% look for a solution which is orthogonal to sin(4*pi*x):
+% look for a solution which is orthogonal to $\sin(4\pi x)$:
 N.bc = @(x,u) sum(sin(4*pi*x).*u);
-u = N\1; plot(u)
+u = N\1; plot(u,LW,1.6), grid on
 %%
 % Again, we obtained an accurate solution (to evaluate the interior point
-% condition, we need to create the chebfun x):
-disp(['Residual of Differ. equation:   ', num2str(norm(N(u)-1))])
-disp(['Residual of Left BC:            ',  num2str(abs(u(-1)-1))])
+% condition, we need to create a chebfun for $x$):
+disp(['Residual of differential equation: ', num2str(norm(N(u)-1))])
+disp(['Residual of left BC:               ',  num2str(abs(u(-1)-1))])
 x = chebfun('x');
-disp(['Residual of Interior Condition: ', num2str(abs(sum(sin(4*pi*x).*u)))])
+disp(['Residual of interior condition:    ', num2str(abs(sum(sin(4*pi*x).*u)))])
+
 %% Example 2: Interior point conditions
 % We now wish to solve the same ODE as above:
 %
-%     u'' + x.^2*u = 1
+% $$ u" + x^2 u = 1 $$
 %
-% on the domain [-1,1], but in addition to a left boundary condition
+% on the domain $[-1,1]$, but in addition to a left boundary condition
 %
-%     u(-1) = 1
+% $$  u(-1) = 1 $$
 %
 % we now have the interior point condition
 %
-%     u(0) = .5;
+% $$  u(0) = 0.5. $$
 
 %%
-% We reuse the chebop from above, and only need to change the .bc field:
+% We reuse the chebop from above, only needing to change the `.bc` field:
 N.bc = @(x,u) u(0)-.5;
+
 %%
 % We solve the problem and confirm that we obtain an accurate solution
-u = N\1; plot(u)
-disp(['Residual of Differ. equation:   ', num2str(norm(N(u)-1))])
-disp(['Residual of Left BC:            ', num2str(abs(u(-1)-1))])
-disp(['Residual of Interior Condition: ', num2str(abs(u(0)-.5))])
+u = N\1; plot(u,LW,1.6), grid on
+disp(['Residual of differential equation: ', num2str(norm(N(u)-1))])
+disp(['Residual of left BC:               ', num2str(abs(u(-1)-1))])
+disp(['Residual of interior condition:    ', num2str(abs(u(0)-.5))])
 
 %%
 % If we want to impose conditions on the derivative, such as
 %
-%     u'(0) = 1;
+% $$ u'(0) = 1, $$
 %
 % we need to use a little trick in order to be able to evaluate the
 % derivative at an interior point. MATLAB doesn't allow the stacked
-% parentheses we would need for something like @(x,u) diff(u)(0), so
-% instead this is achived using the feval method:
+% parentheses we would need for something like `@(x,u) diff(u)(0)`, so
+% instead this is achived using the `feval` method:
 N.bc = @(x,u) feval(diff(u),0)-1;
+
 %%
 % We solve the problem
-u = N\1; plot(u)
+u = N\1; plot(u,LW,1.6), grid on
 
 %%
 % and confirm that we obtain an accurate solution
-disp(['Residual of Differ. equation:   ', num2str(norm(N(u)-1))])
-disp(['Residual of Left BC:            ', num2str(abs(u(-1)-1))])
+disp(['Residual of differential equation: ', num2str(norm(N(u)-1))])
+disp(['Residual of left BC:               ', num2str(abs(u(-1)-1))])
 up = diff(u);
-disp(['Residual of Interior Condition: ', num2str(abs(up(0)-1))])
+disp(['Residual of interior condition:    ', num2str(abs(up(0)-1))])
--- ode/Orbits.m
+++ ode/Orbits.m
@@ -7,15 +7,15 @@
 
 %%
 % Suppose a "star" of unit mass is fixed at the origin in the
-% x-y plane, and
+% $x$ - $y$ plane, and
 % a planet, also of unit mass, moves around it according
-% to Newton's laws with gravitational constant 1.  To be
-% specific, let's suppose the planet starts at (-1,1) heading east
-% with speed v=1.  What does the trajectory look like?
+% to Newton's laws with gravitational constant $1$.  To be
+% specific, let's suppose the planet starts at $(-1,1)$ heading east
+% with speed $v=1$.  What does the trajectory look like?
 
 %%
-% Here is a code to solve this problem using Chebfun's ode113 command,
-% and complex arithmetic for simplicity.  We track the orbit for 23
+% Here is a code to solve this problem using Chebfun's `ode113` command,
+% and complex arithmetic for simplicity.  We track the orbit for $23$
 % time units and see that it is an ellipse.
 tmax = 23; d = [0 tmax];
 LW = 'linewidth'; MS = 'markersize';
@@ -29,12 +29,12 @@ plot(uv,LW,1.6), axis equal, grid on, shg
 plot(uv(0:tmax),'.k',MS,16)
 
 %%
-% If we want the initial speed v to be a parameter, we can make
+% If we want the initial speed $v$ to be a parameter, we can make
 % an anonymous function:
 u = @(v) chebfun.ode113(fun,d,[u0; v],opts);
 
 %%
-% Here are the orbits for v = .5, .75, 1, 1.5, 2.  This kind of
+% Here are the orbits for $v = .5, .75, 1, 1.5, 2$.  This kind of
 % thing is familiar from introductory physics: every orbit is an
 % ellipse, a parabola, or a hyperbola.
 hold off, plot(0,0,'.r','markersize',24), hold on
@@ -47,9 +47,9 @@ axis([-3 3 -3 3]), axis square, grid on, shg
 %%
 % More unusual behavior comes about if we imagine two or more fixed
 % "stars".  Orbits can now be bounded without being
-% periodic.  For example, suppose we have one star at (0,0) and
-% another at (1,0), with the planet feeling a gravitational tug from
-% each.  Here is an orbit over 10 time units starting with v=1:
+% periodic.  For example, suppose we have one star at $(0,0)$ and
+% another at $(1,0)$, with the planet feeling a gravitational tug from
+% each.  Here is an orbit over $10$ time units starting with $v=1$:
 fun = @(t,u) [u(2); -u(1)./abs(u(1)).^3-(u(1)-1)./abs(u(1)-1).^3];
 tmax = 10; d = [0 tmax];
 u = @(v) chebfun.ode113(fun,d,[u0; v],opts);
@@ -59,7 +59,7 @@ plot(uv,LW,1.6), axis equal, grid on, shg
 plot(uv(0:tmax),'.k',MS,16)
 
 %%
-% Here is what happens when the initial speed is reduced to 0.9:
+% Here is what happens when the initial speed is reduced to $0.9$:
 v = 0.9; uv = u(v); uv = uv(:,1);
 hold off, plot([0 1],[0 0],'.r','markersize',24), hold on
 plot(uv,LW,1.6), axis equal, grid on, shg
@@ -74,4 +74,4 @@ orbit_length = norm(diff(uv),1)
 closeness = min(abs(uv))
 
 %%
-% Variations on this theme are infinite!
+% Variations on these themes are infinite!
