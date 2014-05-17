f47dc5f1b80410e00f0408e489e0b2f2457ab995
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-05-17 17:04:49 +0100

    Polishing up example complex/Arguments.m

--- complex/Arguments.m
+++ complex/Arguments.m
@@ -3,13 +3,12 @@
 
 %%
 % (Chebfun example complex/Arguments.m)
-% [Tags: #complex, #angle]
-
+% [Tags: #complex, #ANGLE, #UNWRAP]
 
 %%
-% A complex number z has a modulus or
-% absolute value in [0,infty), which Matlab computes with ABS(z),
-% and an argument in (-pi,pi], which Matlab computes with ANGLE(z).
+% A complex number $z$ has a modulus or
+% absolute value in $[0,\infty)$, which MATLAB computes with `abs(z)`,
+% and an argument in $(-\pi,\pi]$, which MATLAB computes with `angle(z)`.
 % For example:
 angle(1)
 %%
@@ -18,8 +17,8 @@ angle(-1)
 angle(-1-.01i)
 
 %%
-% Chebfun overloads the ANGLE command in the obvious fashion, 
-% analogous to CEIL, FLOOR, and ROUND.  For example, here is
+% Chebfun overloads the `angle` command in the obvious fashion, 
+% analogously to `ceil`, `floor`, and `round`.  For example, here is
 % a spiral in the complex plane:
 LW = 'linewidth'; lw = 1.6; FS = 'fontsize'; fs = 14;
 t = chebfun('t',[1 20]);
@@ -34,7 +33,7 @@ xlabel t, ylabel angle(f(t))
 
 %%
 % Often one would prefer to define a continuous argument, and
-% for this purpose Matlab has the command UNWRAP.  For example:
+% for this purpose MATLAB has the command `unwrap`.  For example:
 unwrap(angle([-1 -1-.01i]))
 
 %%
@@ -48,7 +47,7 @@ xlabel t, ylabel argument
 % An important area of application of these commands is to functions
 % in the complex plane, where keeping track of branch
 % cuts is often a headache.  For example, suppose we want to take
-% the square root of that function f.  The result is not very useful.
+% the square root of that function $f$.  The result is not very useful.
 % (For the moment we have to construct the function again with
 % 'splitting on' to make this experiment work, though in
 % principle Chebfun should be clever enough to introduce a breakpoint
@@ -58,7 +57,7 @@ plot(g,LW,lw), axis(5*[-1 1 -1 1]), axis square
 title('sqrt(f(t)) in complex plane',FS,fs)
 
 %%
-% We can get the right effect with UNWRAP:
+% We can get the right effect with `unwrap`:
 g = sqrt(abs(f)).*exp(.5i*unwrap(angle(f)));
 plot(g,LW,lw), axis(5*[-1 1 -1 1]), axis square
 title('sqrt(f(t)) in complex plane',FS,fs)
