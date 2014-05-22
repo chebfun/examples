4a3fea943b265f9718c7285e5e28ba073b9dde58
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-05-22 10:13:36 +0100

    *not* yet polished - GammaFun.m

--- approx/GammaFun.m
+++ approx/GammaFun.m
@@ -8,15 +8,14 @@
 %%
 % This script displays some of the features introduced in version 3 for
 % unbounded functions by demonstrating with the gamma function on the
-% interval [-4 4].
+% interval $[-4 4]$.
 
 %%
-% The gamma function on [-4 4] has simple poles at the negative integers
+% The gamma function on $[-4 4]$ has simple poles at the negative integers
 % and zero. Chebfun can determine the locations and orders of these poles
-% if it is called with the 'blowup' and 'splitting' flags on. The
-% 'exponents' field of the output indicates that each pole is simple, that
-% is, it has a singularity of type x^(-1).
-
+% if it is called with the `'blowup'` and `'splitting'` flags on. The
+% `'exponents'` field of the output indicates that each pole is simple, that
+% is, it has a singularity of type $x^{-1}$.
 LW = 'linewidth'; FS = 'fontsize'; MS = 'markersize';
 gam = chebfun('gamma(x)',[-4 4],'blowup','on','splitting','on')
 plot(gam,'b',LW,1.6), hold on
@@ -27,25 +26,21 @@ title('Gamma function',FS,16)
 % For example, we can: 
 %
 % (1) Find its reciprocal
-
 gam_i = 1./gam;
 
 %%
 % (2) Compute the square root of abs(gamma)
-
 absgam = abs(gam);
 sqrtgam = real(sqrt(absgam));
 
 %%
 % (3) Plot these functions
-
 plot(gam_i,'r', sqrtgam,'-g',LW,1.6)
 legend('\Gamma(x)', '1/\Gamma(x)', 'sqrt(|\Gamma(x)|)')
 title('Various related functions',FS,16)
 
 %%
 % (4) Plot the critical points
-
 [y r] = minandmax(gam,'local');
 [yi ri] = minandmax(gam_i,'local');
 [ys rs] = minandmax(sqrtgam,'local');
