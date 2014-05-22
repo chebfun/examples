1bcd76a0f61e845128ac7e03b58132d637eb085f
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-05-22 10:12:55 +0100

    polished up LorenzAttractor

--- ode/LorenzAttractor.m
+++ ode/LorenzAttractor.m
@@ -1,4 +1,4 @@
-%% Lorenz Attractor in the Complex Plane using RATINTERP
+%% Lorenz Attractor in the Complex Plane using `ratinterp`
 % Marcus Webb, 10th March 2013
 
 %%
@@ -7,10 +7,10 @@
 
 %%
 % In this example we discuss numerical analytic continuation into the 
-% complex plane using the RATINTERP command in order to approximate
-% singularities of solutions to the Lorenz Attractor.
+% complex plane using the `ratinterp` command in order to approximate
+% singularities of solutions to the Lorenz equations.
 
-%% The Lorenz system with real variables
+%% The Lorenz system in real time
 % The Lorenz system is a system of ODEs first studied by Edward Lorenz in
 % the 1960s as a simplified model of convection rolls in the upper
 % atmosphere [2]. The system is as follows:
@@ -19,15 +19,15 @@
 %
 % $$\frac{dy}{dt} = 28 x - y - xz, $$
 %
-% $$\frac{dz}{dt} = -8z/3 + xy. $$
+% $$\frac{dz}{dt} = -{8\over 3}z + xy. $$
 %
 
 %%
-% We can compute a numerical solution on the interval [0,5] using Chebfun's
-% overload of the MATLAB ODE solver, ODE113:
+% We can compute a numerical solution on the interval $[0,5]$ using Chebfun's
+% overload of the MATLAB ODE solver, `ode113`:
 opts = odeset('abstol',1e-13,'reltol',1e-13);
 fun = @(t,u) [10*(u(2)-u(1)); 28*u(1)-u(2)-u(1)*u(3); u(1)*u(2)-(8/3)*u(3)];
-u = chebfun.ode113(fun,[0,5],[-14 -15 20], opts)
+u = chebfun.ode113(fun,[0,5],[-14 -15 20], opts);
 
 %%
 % The solution can be viewed as an autonomous dynamical system in three
@@ -49,9 +49,10 @@ title('Solution to the Lorenz Attractor as Scalar Functions', FS, 14)
 % Here as usual in MATLAB, $x$, $y$ and $z$ are coloured blue, green and red
 % respectively. 
 
-%% The Lorenz system with complex variables
-% In "Complex Singularities and the Lorenz Attractor" [3], the point is
-% made that although the Lorenz attractor is a well known example in
+%% The Lorenz system in complex time
+% In "Complex singularities and the Lorenz attractor" [3],
+% Viswanath and Sahutoglu make the point
+% that although the Lorenz attractor is a well known example in
 % applied mathematics, relatively little is known about its mathematical
 % analysis. It is suggested that a natural way to view the analysis is as a
 % problem in analytic function theory, considering time as a complex 
@@ -87,31 +88,31 @@ title('Solution to the Lorenz Attractor as Scalar Functions', FS, 14)
 % Asymptotically, the singularities behave like poles of orders
 % 1, 2 and 2 for $x$, $y$ and $z$ respectively, because the logarithmic
 % terms are "overpowered" in the limit as $t \rightarrow t_0$. A rational
-% approximation is therefore appropriate for computing the location of
+% approximation is therefore appropriate for computing the locations of
 % the singularities.
 
 %% Analytic continuation via rational interpolation
-% We can compute a rational interpolant of the solution using RATINTERP,
+% We can compute a rational interpolant of the solution using `ratinterp`,
 % which actually computes a least squares approximation to the
 % interpolation problem, removing poles that are likely to be spurious
-% [Sec. 2; 1]. The following command computes rational interpolant of
-% type ($m$, $n$) = (226,40), (248,40) and (242,40), where $m$ is the degree
+% [1, Sec. 2]. The following command computes rational fits of
+% types $(m, n) = (226,40)$, $(248,40)$ and $(242,40)$, where $m$ is the degree
 % of the numerator and $n$ is the degree of the denominator:
-
 [p1,q1,r1,mu1,nu1,poles1] = ratinterp(u(:,1),221,40, 444, [], 1e-12);
 [p2,q2,r2,mu2,nu2,poles2] = ratinterp(u(:,2),241,40, 484, [], 1e-12);
 [p3,q3,r3,mu3,nu3,poles3] = ratinterp(u(:,3),236,40, 473, [], 1e-12);
 
 %%
-% Here we have taken $m$ to be half the degree of the chebfun u and $n$ 
-% some number greater than the number of singularities we expect to find. 
-% We don't actually expect to find 40 singularities, because RATINTERP will
+% Here we have taken $m$ to be half the degree of the chebfun $u$ and $n$ 
+% to be a number greater than the number of singularities we expect to find. 
+% We don't actually expect to find 40 singularities, because `ratinterp` will
 % reduce the degree of the numerator automatically to avoid extra unwanted 
 % poles.
 
 %%
-% Here we have a contour plot of the rational approximants r1, r2, r3, 
-% computed by RATINTERP with the computed locations of singularities 
+% Here we have a contour plot of the rational approximants $r_1$,
+% $r_2$, $r_3$, 
+% computed by `ratinterp` with the computed locations of singularities 
 % plotted as black crosses:
 
 xx = linspace(-0.5,5.5,200);
@@ -146,12 +147,12 @@ plot([0,5],[0,0],'k',LW,1.6)
 % behaviour of the solution on the real line is intimately related to its 
 % complex singularities. On the other hand, it has been proven that the imaginary 
 % parts of any singularity of the Lorenz attractor must have absolute value
-% greater than 0.037 [3].
+% greater than $0.037$ [3].
 
 %%
 % Note that by the Psi-series formula, each of $x$, $y$ and $z$ should have
 % the same singularities. Let's compare our computed values to obtain a 
-% lower bound on the error. The first column in the following is the 
+% lower bound on the error. The first column in the following contains the 
 % computed locations of the singularities of $x$, the second those of $y$ 
 % and the third those of $z$; the final column is the maximum separation in
 % absolute value of these computed locations:
@@ -162,15 +163,14 @@ disp('   poles in x         poles in y         poles in z         max. differenc
 disp([poles1, poles2, poles3, diff(:,1)])
 
 %% 
-% Let $t_0$ denote the location of a singularity, $t_w$ denote the worst 
+% Let $t_0$ denote the location of a singularity and $t_w$ the worst 
 % of our three approximations of it, $t_1$, $t_2$, $t_3$. Then we have 
 % for $i,j = 1,2,3$:
 %
-% $$ |t_i - t_j| \leq |t_0 - t_i| + |t_0- t_j| \leq 2|t_0-t_w| $$
+% $$ |t_i - t_j| \leq |t_0 - t_i| + |t_0- t_j| \leq 2|t_0-t_w|. $$
 
 %%
 % Hence our worst case error for each pole is at least the following:
-
 0.5*diff(:,1)'
 
 %%
@@ -180,22 +180,20 @@ disp([poles1, poles2, poles3, diff(:,1)])
 % the agreement we see here is not bad, although it would be desirable to have 
 % better.
 
-%% RATINTERP tolerance parameter
-% The final argument in RATINTERP is a tolerance parameter, which we set to
-% 1e-12 above. It affects which poles are removed from the approximant
-% [Sec. 2, 1]. The higher the tolerance, the more poles that are removed by
-% RATINTERP. Here is what happens when this robustness is not used at all
+%% `ratinterp` tolerance parameter
+% The final argument in `ratinterp` is a tolerance parameter, which we set to
+% $10^{-12}$ above. This affects which poles are removed from the approximant
+% [Sec. 2, 1]. The higher the tolerance, the more poles are removed by
+% `ratinterp`. Here is what happens when this robustness is not used at all
 % (tolerance = 0):
-
 [p,q,r,mu,nu,poles] = ratinterp(u(:,1),221,40, 444, [], 0);
 poles
 
 %%
 % There are many unwanted, spurious poles. They are paired with roots of 
 % the numerator, but only up to machine precision, so they don't actually
-% cancel each other out. These are sometimes called Froissart Doublets, 
+% cancel each other out. These are sometimes called _Froissart doublets_, 
 % for the astrophysicist Marcel Froissart.
-
 subplot(1,1,1)
 plot(poles, 'or', MS, 8,'markerfacecolor','r')
 hold on, grid on, xlabel 'Re(t)', ylabel 'Im(t)'
@@ -204,27 +202,29 @@ title('Poles and Zeros of the Rational Interpolant', FS, 14)
 
 %%
 % The issues related to the robust form of rational interpolation and least
-% squares approximation used by RATINTERP are discussed in [1] and [Ch. 26, 4]. 
+% squares approximation used by `ratinterp` are discussed in [1] and
+% Chapter 26 of [4]. 
 
 %%
-% REFERENCES: 
+% References: 
 
 %%
-% [1] M. Webb, Computing Complex Singularities of Differential
-% Equations with Chebfun, SIAM Undergraduate Research Online, to appear.
+% [1] M. Webb, Computing complex singularities of differential
+% equations with Chebfun, _SIAM Undergraduate Research Online_, 2013,
+% http://dx.doi.org/10.1137/12S011520.
 
 %% 
-% [2] E. Lorenz, Deterministic Nonperiodic Flow, Journal of the Atmospheric 
-% Sciences, 1963
+% [2] E. Lorenz, Deterministic nonperiodic flow, _Journal of the Atmospheric 
+% Sciences_, 20 (1963), 130-141.
 
 %%
-% [3] D. Viswanath, S. Sahutoglu, Complex Singularities and the Lorenz
-% Attractor, SIAM Review, 2010.
+% [3] D. Viswanath and S. Sahutoglu, Complex singularities and the Lorenz
+% attractor, _SIAM Review_, 52 (2010), 294-314.
 
 %%
-% [4] L. N. Trefethen, Approximation Theory and Approximation Practice,
+% [4] L. N. Trefethen, _Approximation Theory and Approximation Practice_,
 % SIAM, 2013.
 
 %%
-% [5] L.N. Trefethen, Analytic Continuation via Rational Approximation,
+% [5] L.N. Trefethen, Analytic continuation via rational approximation,
 % Chebfun example, 6th March 2013
