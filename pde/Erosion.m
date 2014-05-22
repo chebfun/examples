c21d5aade3f740fa41e5eb769e9edc5b10e34df3
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-05-22 11:59:07 +0100

    Erosion.m *not* polished; issue filed for @tobydriscoll

--- pde/Erosion.m
+++ pde/Erosion.m
@@ -1,72 +1,62 @@
-%% Heat equation via EXPM
+%% Heat equation via `expm`
 % Nick Trefethen, October 2010
 
 %%
 % (Chebfun example pde/Erosion.m)
-% [Tags: #heatequation, #erosion, #linearPDE]
+% [Tags: #heatequation, #erosion, #linearPDE, #EXPM]
 
 %%
 % A well-known PDE problem is the heat equation initial
-% boundary-value problem posed for x in [a,b] and t > 0,
+% boundary-value problem posed for $x\in [a,b]$ and $t>0$,
 %
-%   u_t = u_xx,  u(x,0) = u0(x)
+% $$ u_t = u_{xx}, ~~~~  u(x,0) = u_0(x) $$
 %
 % with suitable boundary conditions.
 % We can regard this as a time-dependent linear process
 %
-%   u_t = Lu
+% $$ u_t = Lu $$
 %
-% where L is the operator d^2/dx^2 on [a,b] with the same
+% where $L$ is the operator $d^2/dx^2$ on $[a,b]$ with the same
 % boundary conditions.  The solution is
 %
-%   u(t) = e^(tL) u(0).
+% $$ u(t) = \exp(tL) u(0). $$
 
 %%
-% In Chebfun we can implement this idea using the EXPM command
+% In Chebfun we can implement this idea using the `expm` command
 % to compute the operator exponential.
 % Here is an example with Neumann boundary conditions on
-% the interval [0,6].  We start with quite an irregular
+% the interval $[0,6]$.  We start with quite an irregular
 % initial function.
-d = [0,6]; 
-u0 = chebfun(@(x) sign((-1).^floor(x.^1.5)),d,'splitting','on');
+d = [0,6]; d = [0 3];
+u = chebfun(@(x) sign((-1).^floor(x.^1.5)),d,'splitting','on');
 LW = 'linewidth'; lw = 2; FS = 'fontsize'; fs = 16;
-clf, plot(u0,LW,lw), grid on
-title(sprintf('t = %4.2f     length = %d',0,length(u0)),FS,fs)
+clf, plot(u,LW,lw), grid on
+title(sprintf('t = %4.2f     length = %d',0,length(u)),FS,fs)
 ax = [0 6 -1.2 1.2]; axis(ax)
 
 %%
-% Here's the solution at t = 0.01.  Notice that the narrower spikes
+% Here's the solution at $t = 0.01$.  Notice that the narrower spikes
 % have lost more amplitude than the wider ones.
-% The warning message is important:
-% Chebfun does not always give its full accuracy for computations of
-% this kind.
 L = chebop(d);          % operator on domain [0,6]
 L.op = @(u) diff(u,2);  % 2nd-derivative operator
 L.lbc = @(u) diff(u);   % Neumann BC at left
 L.rbc = @(u) diff(u);   % Neumann BC at right
 dt = 0.01;
-expmL = expm(dt*L);     % exponential of the operator
-u = expmL*u0;
+u = expm(L,dt,u);
 plot(u,LW,lw), axis(ax), grid on
 title(sprintf('t = %4.2f     length = %d',0.01,length(u)),FS,fs)
 
-
-figure
-
 %%
-% Here is the solution at t = 0.02.  Now that the function is smooth,
-% there are no further warning messages.  The rightmost maximum has extra
+% Here is the solution at $t = 0.02$. The rightmost maximum has extra
 % amplitude, since it effectively corresponded to a wider initial spike
 % thanks to the Neumann boundary condition.
-u = expmL*u;
+u = expm(L,dt,u);
 plot(u,LW,lw), axis(ax), grid on
 title(sprintf('t = %4.2f     length = %d',0.02,length(u)),FS,fs)
 
-figure
-
 %%
-% At t = 0.1, there is not much of the original structure left.  The
+% At $t=0.1$, there is not much of the original structure left.  The
 % length of the chebfun has also been reduced.
-u = expm(0.08*L)*u;
+u = expm(L,0.1-2*dt,u);
 plot(u,LW,lw), axis(ax), grid on
 title(sprintf('t = %4.2f     length = %d',0.1,length(u)),FS,fs)
