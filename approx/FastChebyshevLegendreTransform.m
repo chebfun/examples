e7c4bdab691d034b9e2844ab083c8b4fffcfce52
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-05-23 14:53:21 +0100

    Polished FastChebyshevLegendreTransform.m

--- approx/FastChebyshevLegendreTransform.m
+++ approx/FastChebyshevLegendreTransform.m
@@ -6,16 +6,15 @@
 % [Tags: #fast transform, #Legendre, #Chebyshev, #coefficients,
 % #FFT, #LEGPTS, #LEG2CHEB, #CHEB2LEG]
 
-LW = 'linewidth'; lw = 2; 
-MS = 'markersize'; ms = 20; 
-FS = 'fontsize'; fs = 16;
+LW = 'linewidth'; lw = 1.6; 
+MS = 'markersize'; FS = 'fontsize'; fs = 12;
 
 %% The Chebyshev-Legendre transform
 % Chebfun is based on Chebyshev interpolants and their related fast algorithms.
 % Chebyshev interpolants are a very practical tool for computing with smooth
 % functions. However, in some situations Legendre expansions, i.e.,
 % 
-% $p(x) = \sum_{n=0}^N c_n^{leg} P_n(x),$
+% $$ p(x) = \sum_{n=0}^N c_n^{leg} P_n(x), $$
 %
 % where $P_n(x)$ is the degree $n$ Legendre polynomial, are desirable due to
 % their orthogonality in the standard $L^2$ inner product. Recently, a new
@@ -23,18 +22,18 @@ FS = 'fontsize'; fs = 16;
 % that converts between $N$ Legendre and Chebyshev coefficients in $\mathcal{O}(
 % N (\log N)^2/ \log \log N)$ operations [2]. The algorithm is based on a
 % long-established asymptotic formula for Legendre polynomials [4], which was
-% previously used in Chebfun's LEGPTS() command [1]. The transform comes in two
-% parts: (1) The forward transform, LEG2CHEB(), which converts Legendre to
-% Chebyshev coefficients and (2) The inverse transform, CHEB2LEG(), that
+% previously used in Chebfun's `legpts` command [1]. The transform comes in two
+% parts: (1) The forward transform, `leg2cheb`, which converts Legendre to
+% Chebyshev coefficients and (2) The inverse transform, `cheb2leg`, which
 % converts in the other direction.
 
 %%
-% In this Example a few applications of the transform are considered. 
+% In this Example a few applications are considered. 
 
 %% Computing the Legendre coefficients
 % The problem of 
 % computing the coefficients in a Legendre expansion of a function has received
-% considerable research attention since the 1970s and there are now many
+% considerable research attention since the 1970s, and there are now many
 % approaches (a summary is given [2]). In particular, any fast
 % Chebyshev-Legendre transform can be used to compute the Legendre coefficients
 % of a function by going via Chebyshev coefficients. For example, here are the
@@ -43,30 +42,31 @@ FS = 'fontsize'; fs = 16;
 
 f = chebfun(@(x) 1./(1 + 1000*(x-.1).^2));  % A Runge-type function  
 c_cheb = chebpoly(f).';                     % Chebyshev coeffs in O(NlogN)
-c_leg = chebtech.cheb2leg(c_cheb);                   % Leg coeffs with the new algorithm
+c_leg = chebtech.cheb2leg(c_cheb);          % Leg coeffs with the new algorithm
 
-semilogy(flipud(abs(c_leg)), '-r'), hold on  % plot them
-semilogy(flipud(abs(c_cheb)), '.b', LW, lw)
+semilogy(flipud(abs(c_leg)), 'xr',MS,4), hold on  % plot them
+semilogy(flipud(abs(c_cheb)), '.b', MS,8)
 legend('Legendre coefficients','Chebyshev coefficients')
 xlabel('n', FS, fs), set(gca, FS, fs), hold off
 
 %%
-% For an analytic function the Legendre and Chebyshev coefficients decay at the
+% For an analytic function, the Legendre and Chebyshev coefficients decay at 
+% essentially the
 % same geometric rate. However, for algebraically smooth functions the decay of
 % Legendre coefficients is about $n^{-1/2}$ worse than that of the corresponding
-% Chebyshev coefficients, which has been explained in [5].  Here we witness this
-% for the function $|x-.1|^{7/4}$:
+% Chebyshev coefficients, a phenomenon discussed in [5].  Here we witness this
+% disparity for the function $|x-.1|^{7/4}$:
 
 f = chebfun(@(x) abs(x-.1).^(7/4)); N = length(f);  % |x-.1|^(7/4)
 c_cheb = chebpoly(f)';                              % Chebyshev coeffs
 c_leg = chebtech.cheb2leg(c_cheb);                           % Legendre coeffs
 
-semilogy(flipud(abs(c_leg)), 'r'), hold on,         % plot them
-semilogy(flipud(abs(c_cheb)), 'b'),
+semilogy(flipud(abs(c_leg)), 'xr',MS,4), hold on,         % plot them
+semilogy(flipud(abs(c_cheb)), '.b',MS,8),
 semilogy(1:N,(1:N).^(-7/4-1+.5), 'k--', LW, lw)
 semilogy(1:N,(1:N).^(-7/4-1), 'k--', LW, lw)
 legend('Legendre coefficients','Chebyshev coefficients',...
-                                        'O(n^{-2.25})','O(n^{-2.75})')
+                'O(n^{-2.25})','O(n^{-2.75})')
 xlim([0, N]), xlabel('n', FS, fs), set(gca, FS, fs), hold off
 
 %% 
@@ -75,7 +75,7 @@ xlim([0, N]), xlabel('n', FS, fs), set(gca, FS, fs), hold off
 % for Chebyshev points it grows like $O(\log n)$.
 
 %% Fast evaluation of Legendre expansions
-% Given a Legendre expansion the fast transform can also be used to rapidly
+% Given a Legendre expansion, the fast transform can also be used to rapidly
 % evaluate a Legendre series at Chebyshev points of the second kind. For
 % example,
 
@@ -83,8 +83,8 @@ t = .999i; f = chebfun(@(x) 1./sqrt(1 - 2*x.*t +t.^2)); % generating function
 N = length(f); 
 ns = sprintf('No. of evaluation points = %u\n',N);
 
-s = tic;                                           % evaluate f
-c_leg = t.^(N-1:-1:0);                             % via Legendre coeffs
+s = tic;                                                % evaluate f
+c_leg = t.^(N-1:-1:0);                                  % via Legendre coeffs
 cheb_vals = chebtech2.coeffs2vals(chebtech.leg2cheb(c_leg)); % and time it...
 tt = toc(s);
 
@@ -96,17 +96,18 @@ title('Absolute error', FS, fs), hold off
 axis([-1 1 1e-16 1e-12]), set(gca, FS, fs), hold off
 
 %% Computing Legendre coefficients by a spectral method
-% Recently, the ultraspherical spectral method was developed that solves linear
+% Recently, an ultraspherical spectral method was developed, which solves linear
+% constant-coefficient
 % ODEs in essentially $O(N)$ operations and computes the Chebyshev coefficients
-% of the solution [3]. However, the approach easily generalises to a fast
-% Legendre spectral method. Moreover, now we have a fast transform we can
+% of the solution [3]. This approach easily generalises to a fast
+% Legendre spectral method. Now that we have a fast transform, we can
 % rapidly construct a chebfun object from the Legendre coefficients.
 
 %% 
-% Let's solve a linear ODE that requires about N = 32000 Legendre coefficients
+% Let's solve a linear ODE that requires about $N=32000$ Legendre coefficients
 % to resolve the solution. The ODE is
 %
-% $u''(x) + (10000\pi)^2u = 0, \qquad u(-1) = u(1) = 1,$ 
+% $$ u''(x) + (10000\pi)^2u = 0, \qquad u(-1) = u(1) = 1, $$ 
 % 
 % and hence, the solution is $\cos(10000\pi x)$. 
 
@@ -127,8 +128,7 @@ c_leg = flipud( (P*A) \ (P*b) );               % solve
 toc
 
 %%
-% We can now form a chebfun from solution using LEG2CHEB():
-
+% We can now form a chebfun from solution using `leg2cheb`:
 tic
 c_cheb = chebtech.leg2cheb(c_leg);
 u = chebfun(c_cheb, 'coeffs');                 
@@ -139,30 +139,31 @@ title('Computed solution (zoomed in)', FS, fs)
 set(gca, FS, fs), xlabel('x', FS, fs), shg
 
 %% 
-% Here is the error between the computed solution and the true solution:
-
+% Here is the error between the computed and the true solutions:
 norm(c_cheb - chebpoly(f).', inf)
 norm(u - f)
 
 %% Conclusion
-% We believe that such a fast and practical Chebyshev-Legendre transform could
-% allow the Chebfun project to occasionally compute with Legendre
-% representations. This could simplify some algorithms and lead to the
-% development of others.
+% The existence of a fast and practical Chebyshev-Legendre transform has
+% many implications.  Within Chebfun, it has led to major speedups
+% in the commands `conv` and `polyfit`.  Further developments are in
+% the pipeline.
 
 %% References 
-%%
+%
 % [1] N. Hale and A. Townsend, Fast and accurate computation of Gauss-Legendre
-% and Gauss-Jacobi quadrature nodes and weights, SISC, 35 (2013), A652-A672.
-%%
+% and Gauss-Jacobi quadrature nodes and weights, 
+% _SIAM Journal on Scientific Computing_, 35 (2013), A652-A672.
+%
 % [2] N. Hale and A. Townsend, A fast, simple, and stable Chebyshev--Legendre
-% transform using an asymptotic formula, submitted, 2013.
-%%
+% transform using an asymptotic formula, _SIAM Journal on Scientific
+% Computing_, 36 (2014), A148-A167.
+%
 % [3] S. Olver and A. Townsend, A fast and well-conditioned spectral method, to
-% appear in SIAM Review, 2013.
-%%
-% [4] T. J. Stieltjes, Sur les polynomes de Legendre, Annales de faculte des
-% sciences de Toulouse, 4 (1980), G1-G17.
-%% 
+% appear in _SIAM Review_, 55 (2013), 462-489
+%
+% [4] T. J. Stieltjes, Sur les polynomes de Legendre, _Annales de Faculte des
+% Sciences de Toulouse_, 4 (1890), G1-G17.
+% 
 % [5] H. Wang and S. Xiang, On the convergence rates of Legendre approximation,
-% Math. Comp., 81 (2012), pp.861-877.
+% _Mathematics of Computation_, 81 (2012), 861-877.
