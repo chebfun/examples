3f71f3542d147e2172e274e2ada2de90dc652acd
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-06-14 13:48:49 +0100

    BoundaryLayer is polished (despite issue #843)

--- ode/BoundaryLayer.m
+++ ode/BoundaryLayer.m
@@ -8,13 +8,13 @@
 %%
 % Consider the steady-state linear advection-diffusion equation
 %
-% L_eps u = -eps u" - u' = 1,    u(0) = u(1) = 0 ,
+% $$ L_\varepsilon u = -\varepsilon u" - u' = 1,\qquad    u(0) = u(1) = 0 , $$
 %
-% where eps is a small positive parameter.
-% The solution to this equation has a boundary layer near x=0.
+% where $\varepsilon>0$ is a small parameter.
+% The solution to this equation has a boundary layer near $x=0$.
 
 %%
-% In Chebfun, we can define the eps-dependent operator like this:
+% In Chebfun, we can define the $\varepsilon$-dependent operator like this:
 dom = [0,1];
 L = @(eps) chebop(@(u) -eps*diff(u,2) - diff(u),dom,'dirichlet');
 
@@ -24,31 +24,31 @@ L = @(eps) chebop(@(u) -eps*diff(u,2) - diff(u),dom,'dirichlet');
 L = @(eps) chebop(@(u) -eps*diff(u,2) - diff(u),dom) & 'dirichlet';
 
 %%
-% For eps=0.1 we get this picture:
+% For $\varepsilon=0.1$ we get this picture:
 u = L(0.1)\1;
 LW = 'linewidth'; lw = 1.6;
 clf, plot(u,'b',LW,lw)
 grid on, axis([-0.03 1 0 1.03])
 
 %%
-% Let's add a curve for eps = 0.01:
+% Let's add a curve for $\varepsilon = 0.01$:
 u = L(0.01)\1;
 hold on, plot(u,'r',LW,lw)
 
 %%
-% Here's eps = 0.001:
+% Here's $\varepsilon = 0.001$:
 u = L(0.001)\1;
 hold on, plot(u,LW,lw,'color',[0 .8 0])
 legend('\epsilon=0.1','\epsilon=0.01','\epsilon=0.001')
 FS = 'fontsize';
-title('Boundary layers for three values of \epsilon',FS,16)
+title('Boundary layers for three values of \epsilon',FS,12)
 
 %%
 % It can be shown that the width of the boundary layer
-% for this equation is O(eps).  Suppose we want to measure this
+% for this equation is $O(\varepsilon)$.  Suppose we want to measure this
 % in Chebfun.  One method would be to find the point where the 
-% solution goes through 0.5.  (This definition wouldn't work for
-% larger eps.)
+% solution goes through $0.5$.  (This definition wouldn't work for
+% larger $\varepsilon$.)
 width = @(eps) min(roots(L(eps)\1-.5));
 
 %%
@@ -63,7 +63,7 @@ MS = 'markersize';
 plot(w,[.5 .5 .5],'.k',MS,18)
 
 %%
-% We can also plot boundary layer width against epsilon.
+% We can also plot boundary layer width against $\varepsilon$.
 % The dashed red line confirms the linear behavior.
 epsvec = [.1 .03 .01 .003 .001 .0003];
 for j = 1:length(epsvec)
@@ -71,6 +71,6 @@ for j = 1:length(epsvec)
 end
 clf
 loglog(epsvec,w,'.-k',LW,1.6,MS,16), grid on
-xlabel('\epsilon',FS,16)
-ylabel('width of boundary layer',FS,16)
+xlabel('\epsilon',FS,12)
+ylabel('width of boundary layer',FS,12)
 hold on, plot(epsvec,epsvec,'--r',LW,2)
