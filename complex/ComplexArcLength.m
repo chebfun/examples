43748616a5493ca843795dffe5505ce0c03af333
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-06-15 22:31:03 +0100

    ComplexArcLength is polished

--- complex/ComplexArcLength.m
+++ complex/ComplexArcLength.m
@@ -3,63 +3,56 @@
 
 %%
 % (Chebfun example complex/ComplexArcLength.m)
-% [Tags: #complex, #arclength, #roots]
+% [Tags: #complex, #arcLength, #ROOTS, #INV]
 
-%% 1. ARC LENGTH OF A CONTOUR
-% Here is a contour discussed in one of the Example "A Keyhole
+%% 1. Arc length of a contour
+% Here is a contour discussed in the Example "A Keyhole
 % Contour Integral" [1]: 
-
 r = 0.2; R = 2; e = 0.1;
-t = chebfun('t',[0 1]);                 % Parameter
+t = chebfun('t',[0 1]);                   % parameter
 c = [-R+e*1i -r+e*1i -r-e*1i -R-e*1i];
-z = [ c(1) + t*(c(2)-c(1))              % Top of the keyhole
-      c(2)*c(3).^t ./ c(2).^t           % Inner circle
-      c(3) + t*(c(4)-c(3))              % Bottom of the keyhole
-      c(4)*c(1).^t ./ c(4).^t];         % Outer circle
+z = join( c(1) + t*(c(2)-c(1)), ...       % top of the keyhole
+          c(2)*c(3).^t ./ c(2).^t, ...    % inner circle
+          c(3) + t*(c(4)-c(3)), ...       % bottom of the keyhole
+          c(4)*c(1).^t ./ c(4).^t);       % outer circle
 LW = 'LineWidth'; lw = 1.6;
-plot(z,LW,lw), axis equal, xlim([-2.8 2.8])
+plot(z,LW,lw), axis equal
 
 %%
 % The total length of the contour can be calculated with one command: 
-
-L = arclength(z)
+L = arcLength(z)
 
 %%
 % If the length of each patch is what you want to know, then you can make
 % the input a quasimatrix whose columns are the different pieces.
-
 z = [ c(1) + t*(c(2)-c(1))...           % Top of the keyhole
       c(2)*c(3).^t ./ c(2).^t...        % Inner circle
       c(3) + t*(c(4)-c(3))...           % Bottom of the keyhole
       c(4)*c(1).^t ./ c(4).^t];         % Outer circle
+L = arcLength(z)
 
-L = arclength(z)
-
-%% 2. EQUIDISTRIBUTING POINTS ALONG A CONTOUR
+%% 2. Equidistributing points along a contour
 % It is not uncommon that we need to discretize and sample over a 2D curve
 % in the complex plane. For instance, we may want to uniformly distribute 
 % points along the boundary of a domain when the boundary integral method is 
 % used. That is, the points should be equally-spaced with respect
 % to arc length. Consider the following star-shaped curve:
-
 t = chebfun('t',[0 1]); 
 s = exp(1i*2*pi*t).*(0.5*sin(8*pi*t).^2+0.5);
 plot(s,LW,lw), axis equal, hold on
 
 %%
 % First, we calculate the total arc length of this closed curve.
-
-L = arclength(s)
+L = arcLength(s)
 
 %%
 % Suppose, for example, that we want to equidistribute 64 points.
-
 N = 64;
 h = L/N;
 T = zeros(1,N);
 
 %%
-% We can locate the points by solving 63 rootfinding problems:
+% We could do this by solving 63 rootfinding problems:
 tic
 len = cumsum(abs(diff(s)));
 for k = 1:N-1
@@ -68,24 +61,26 @@ end
 toc
 
 %%
-% Now we have the coordinates of the points, and let's mark them on the
+% Now that we have the coordinates of the points, and let's mark them on the
 % curve.
-
 P = s(T);
-MS = 'MarkerSize'; ms = 20;
+MS = 'MarkerSize'; ms = 16;
 plot(P,'.r',MS,ms)
 
 %%
-% The rootfinding problems above, unfortunately, took a number of seconds,
-% because the chebfun len is rather long:
+% The rootfinding problems above, unfortunately, took quite a while,
+% because the chebfun `len` is rather long:
 length(len)
 
 %%
-% In principle, what we ought to do for this problem is first invert len 
-% with the Chebfun commands INV or INV2, and then evaluate the result at 63
-% equally spaced points.  However, INV and INV2 do not currently succeed at
-% this, though INV2 can successfully invert on a subinterval, e.g. with the
-% command g = inv2(len{0,0.1}). Further research is needed!
+% A better approach is not to solve our own rootfinding problems but to
+% first invert `len` with the Chebfun command `inv`:
+tic, g = inv(len); toc
+
+%%
+% Here we put circles around the dots to confirmthat we have the same
+% result as before:
+plot(s(g((0:N-1)*h)),'ok',MS,8)
 
 %% References
 %
