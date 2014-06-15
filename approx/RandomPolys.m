f8ab7d4c8f7d7f797b5dd782d2cc913be1e18d3f
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-06-15 20:14:44 +0100

    various improvements to RandomPolys.m

--- approx/RandomPolys.m
+++ approx/RandomPolys.m
@@ -1,9 +1,9 @@
-%% Roots of random polynomials
+%% Roots of random polynomials on an interval
 % Nick Trefethen, June 2014
 
 %%
-% (Chebfun example approx/RandomPolys.m)
-% [Tags: #Das, #LEG2CHEB, #CHEB2LEG, #ROOTS]
+% (Chebfun example roots/RandomPolys.m)
+% [Tags: #Das, #LEG2CHEB, #ROOTS]
 
 %%
 % Recently I heard a talk by Igor Pritsker of Oklahoma State
@@ -12,15 +12,15 @@
 % real polynomials [1].  This can be very nicely illustrated in Chebfun.
 
 %%
-% Das's result asserts that for a random polynomial with
-% real coefficients, the fraction of roots that are real will be about
-% $1/\sqrt 2 \approx 0.57735$.
-% Specifically, this fraction appears (with probability $1$)
-% asymptotically in the limit $n\to\infty$, where $n$ is the
-% degree, assuming the random polynomials are defined as sums of Legendre
-% polynomials with independent coefficients from the standard
-% normal distribution.  The Legendre polynomials should be normalized
-% by having 2-norm equal to $1$, not by taking the value $1$ at $x=1$.
+% Das's result asserts that for a random polynomial on $[-1,1]$ with
+% real coefficients, the fraction of roots that lie in $[-1,1]$
+% will be about $1/\sqrt 3 \approx 0.57735$.
+% By a random polynomial on $[-1,1]$, we mean a linear combination of
+% Legendre polynomials (normalized by 2-norm on $[-1,1]$)
+% with random independent coefficients 
+% drawn from the standard normal distribution.
+% For such polynomials, the fraction of roots in $[-1,1]$
+% approaches $1/\sqrt 3$ as $n\to\infty$ with probability 1.
 
 %%
 % Here for example is a random polynomial of degree 30:
@@ -31,36 +31,35 @@ ccheb = leg2cheb(cleg,'normalization');        % Chebyshev coeffs
 p = chebfun(ccheb,'coeffs');
 LW = 'linewidth'; FS = 'fontsize'; MS = 'markersize';
 plot(p,LW,1.6), axis([-1.1 1.1 -n n]), grid on
-r = roots(p,'all');
-rr = r(imag(r)==0);
+rr = roots(p);
 hold on, plot(rr,p(rr),'.r',MS,16), hold off
 ratio = length(rr)/n;
 title(['fraction of real roots = ' num2str(ratio)],FS,12)
 
 %%
-% You might guess by looking at this picture that this was a
-% polynomial of odd degree, but of course it is of even degree.
-% So there must be a root outside $[-1,1]$, and we can confirm this
-% by comparing
-length(rr)
-%%
-% and 
-length(roots(p))
-%%
-% Here is the rogue root:
-rr(abs(rr)>1)
+% Here are its roots in the complex plane, both real and complex:
+r = roots(p,'all');
+plot([-1 1],[0 0],'k',LW,1), grid on
+hold on, plot(r,'.r',MS,12), hold off
+xlim([-2.5 2.5]), axis equal
+set(gca,'xtick',-2:2)
 
 %%
-% Now let's consider ten random polynomials of degree 500 and print
+% You might guess by looking at the first of these pictures that this was a
+% polynomial of odd degree, but of course it is of even degree.
+% So there must be a real root outside $[-1,1]$, as indeed
+% we see in the second picture.
+
+%%
+% Now let's construct ten random polynomials of degree 1000 and print
 % the fraction of real roots for each:
-n = 500;
+n = 1000;
 data = [];
 for k = 1:10
   cleg = randn(n+1,1);                         % Legendre coeffs
   ccheb = leg2cheb(cleg,'normalization');      % Chebyshev coeffs
   p = chebfun(ccheb,'coeffs');
-  r = roots(p,'all');
-  rr = r(imag(r)==0);
+  rr = roots(p);
   ratio = length(rr)/n;
   data = [data ratio];
   disp(['fraction of real roots = ' num2str(ratio)])
@@ -70,7 +69,13 @@ end
 % The mean for the whole experiment is pretty close to $0.577$,
 mean(data)
 
-%% References
+%%
+% One could vary these experiments in all kinds of ways, for example
+% defining random polynomials via Chebyshev expansions or by interpolation
+% of random data in Chebyshev or Legendre points.
+
+%%
+% References:
 %
 % 1. M. Das, Real zeros of a random sum of orthogonal polynomials,
 %     _Proceedings of the American Mathematical Society_, 27
