e6ed104abb05fec0136af61f1303fbe8b71efd6f
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-06-15 10:37:53 +0100

    DynamicalSystems is not yet working

--- veccalc/DynamicalSystems.m
+++ veccalc/DynamicalSystems.m
@@ -7,21 +7,23 @@
 
 %%
 % A linear dynamical system in $\mathbf{R}^2$ can be written as
-% $$ x'(t) = A x(t), \qquad x(0) = x_0,$$
+%
+% $$ x'(t) = A x(t), \qquad x(0) = x_0, $$
+%
 % where $A$ is a $2\times 2$ matrix. If $\lambda_1$ and $\lambda_2$ are the
 % eigenvalues of $A$ (assuming that $A$ is diagonalizable) with eigenvectors
 % $v_1$ and $v_2$, then the solution is given by
-% $$ x(t) = \alpha_1 e^{\lambda_1 t} v_1 + \alpha_2 e^{\lambda_2 t} v_2.$$
+%
+% $$ x(t) = \alpha_1 e^{\lambda_1 t} v_1 + \alpha_2 e^{\lambda_2 t} v_2. $$
+%
 % The solution thus depends heavily on the eigenvalues and eigenvectors of $A$;
 % see also [1] and [2].
 % If both eigenvalues have positive real part, then the solution must
 % diverge. The following code uses Chebfun2 first to plot the phase plane,
 % then to plot some individual trajectories. In Chebfun2, the phase plane is given by
-% a chebfun2v object and the trajectories can be computed with ode45.
+% a chebfun2v object and the trajectories can be computed with `ode45`.
 % In each plot, the initial value of each solution is marked with a dot.
-
 LW = 'linewidth'; FS = 'fontsize'; MS = 'markersize';
-
 g = chebfun2v(@(x,y) x, @(x,y) y);    % identity chebfun2v
 A = [2 -2;0 1]; [EV, EW] = eig(A);    % matrix of the system and eigenvalues
 G = A*g; T = [0 3];                   % phase plane and time interval
@@ -41,14 +43,12 @@ end
 plot(0,0,'k.',MS,20), hold off
 title('The origin is an unstable fixed point',FS,14)
 
-
 %%
 % Since at least one of the eigenvalues has positive real part, the phase
 % portrait has an unstable fixed point at the origin.
 % If both eigenvalues have nonpositive real part, then the solution can not
 % grow infinitely large in absolute value. The origin is here a stable fixed
 % point, all trajectories approach that point.
-
 A = [-1 3; 0 -3]; [EV, EW] = eig(A);
 fprintf('eigenvalues of A:\n'), disp(diag(EW)')
 fprintf('eigenvectors of A:\n'), disp(EV)
@@ -85,7 +85,9 @@ title('The origin is a center',FS,14)
 % $\mathrm{tr}(A) = \lambda_1 + \lambda_2 =: \tau$ and the determinant
 % $\mathrm{det}(A) = \lambda_1 \lambda_2 =: \Delta$ of the matrix $A$,
 % since the eigenvalues of are given by
+%
 % $$ \lambda_{1,2} = \frac12(\tau \pm \sqrt{\tau^2 - 4\Delta}). $$
+%
 % All cases can be summarized in the following picture, where the parabola
 % is defined by $\tau^2 - 4\Delta = 0$.
 
@@ -102,7 +104,8 @@ title('Stability of linear dynamical systems',FS,14)
 xlabel('det(A)',FS,14), ylabel('tr(A)',FS,14), hold off
 
 %%
-% So far we have seen systems with a stable and an unstable fixed point, and one with
+% So far we have seen systems with a stable and an
+% unstable fixed point, and one with
 % a center, which occurred since the corresponding matrix $A$ has zero trace
 % and positive determinant. This is the borderline between stable and unstable
 % spirals. Here is a system with an unstable spiral, which corresponds to $A$
@@ -126,7 +129,6 @@ title('The origin is an unstable spiral',FS,14)
 % A system with a stable spiral, which corresponds to $A$
 % having negative trace $\tau$ and positive determinant $\Delta$ with
 % $\tau > -2\sqrt{\Delta}$:
-
 A = [-.5 -2;2 -.2]; [EV, EW] = eig(A);
 fprintf('eigenvalues of A:\n'), disp(diag(EW)')
 fprintf('eigenvectors of A:\n'), disp(EV)
@@ -143,7 +145,6 @@ title('The origin is a stable spiral',FS,14)
 %%
 % A system with a saddle, which corresponds to $A$
 % having negative determinant:
-
 A = [1 1; 4 -2]; [EV, EW] = eig(A);
 fprintf('eigenvalues of A:\n'), disp(diag(EW)')
 fprintf('eigenvectors of A:\n'), disp(EV)
@@ -161,7 +162,6 @@ title('The origin is a saddle point',FS,14)
 %%
 % A system with a line of stable fixed points, which corresponds to $A$
 % having negative trace and zero determinant:
-
 A = [1 1; -2 -2]; [EV, EW] = eig(A);
 fprintf('eigenvalues of A:\n'), disp(diag(EW)')
 fprintf('eigenvectors of A:\n'), disp(EV)
@@ -178,7 +178,6 @@ title('A line of stable fixed points',FS,14)
 %%
 % A system with a line of unstable fixed points, which corresponds to $A$
 % having positive trace and zero determinant:
-
 A = [1 2; 1 2]; [EV, EW] = eig(A);
 fprintf('eigenvalues of A:\n'), disp(diag(EW)')
 fprintf('eigenvectors of A:\n'), disp(EV)
@@ -215,7 +214,8 @@ plot(0,0,'k.',MS,20), hold off
 title('A stable node and collinear eigendirections',FS,14)
 
 %%
-% A system with an unstable node and collinear eigendirections, which corresponds
+% A system with an unstable node and collinear
+% eigendirections, which corresponds
 % to $A$ having positive trace $\tau$ and positive determinant $\Delta$ with
 % $\tau = 2\sqrt{\Delta}$, so that both eigenvalues are equal:
 
@@ -234,7 +234,7 @@ title('An unstable node and collinear eigendirections',FS,14)
 
 %% References
 %
-% 1. R. Abraham and J. E. Marsden, Foundations of Mechanics, Benjamin-
+% 1. R. Abraham and J. E. Marsden, _Foundations of Mechanics_, Benjamin-
 %    Cummings, 1978.
 %
-% 2. S. H. Strogatz, Nonlinear Dynamics and Chaos, Addison-Wesley, 1994.
+% 2. S. H. Strogatz, _Nonlinear Dynamics and Chaos_, Addison-Wesley, 1994.
