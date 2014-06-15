7c6be5bc4890283d30be1aaa3f4889d968bf9782
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-06-15 22:04:38 +0100

    Hyperfuns.m is now polished

--- complex/Hyperfuns.m
+++ complex/Hyperfuns.m
@@ -6,42 +6,43 @@
 % [Tags: #DIRAC, #complex, #deltafunction, #heaviside, #hyperfunction, #delta]
 
 %% Introduction
-% Hyperfunction theory describes a generalized function f(x) on the real
+% Hyperfunction theory describes a generalized function $f(x)$ on the real
 % line by viewing it as the difference between two generating functions
-% F+(z) and F-(z), holomorphic in the upper and lower complex plane,
-% respectively. The value of f(x) at a "regular point" x on the real line
-% is calculated by taking the difference of the upper and lower part of the
-% generating functions and then applying the limit:
-%%
-% limit ep-->0 [ F+(x+i*ep) - F-(x-i*ep) ]
-%%
-% i.e. we approach the real-axis from directly above and directly below the
-% point x. If the limit does not exist, x is called a "singular point" of f
-% and it does not make sense to talk about the value of f at x [1].
+% $F_+(z)$ and $F_-(z)$, holomorphic in the upper and lower complex planes,
+% respectively. The value of $f(x)$ at a _regular point_ $x$ on the real line
+% is calculated by taking the difference of the upper and lower parts of the
+% generating functions and then applying the limit,
+%
+% $$ \lim_{\varepsilon\to 0} [ F_+(x+i\varepsilon) - F_-(x-i\varepsilon) ] $$
+%
+% i.e., we approach the real axis from directly above and below the
+% point $x$. If the limit does not exist, $x$ is called
+% a _singular point_ of $f$
+% and it does not make sense to talk about the value of $f$ at $x$ [1].
 
 %%
-% (A given hyperfunction does not have a unique choice of generators; you
-% can add any analytic function to F+ and F- without changing their
+% A given hyperfunction does not have a unique choice of generators; you
+% can add any analytic function to $F_+$ and $F_-$ without changing their
 % difference along the real axis.  So properly speaking, a hyperfunction is
-% an equivalence class.)
+% an equivalence class.
 
-%% The Dirac-Delta Function
-% An elegant choice of generating function for the Dirac-delta function is to
-% take both F- and F+ equal to the same function,
+%% The Dirac delta function
+% An elegant choice of generating function for the Dirac delta function is to
+% take both $F_-$ and $F_+$ equal to the same function,
 F = @(z) -1./(2i*pi*z);
 
 %%
-% Thus in this case F+ = F-.
+% Thus in this case $F_+ = F_-$.
 
-%% The Heaviside Function
+%% The Heaviside function
 % The Heaviside function also has an elegant choice of generator, again
-% with G- = G+ equal to the same function, the integral of the previous
+% with $G_- = G_+$ equal to the same function, the integral of the previous
 % one:
 G = @(z) -1/(2*pi*1i)*log(-z);
 
 %%
 % Using the above definitions, we define anonymous functions to describe
-% the delta function and the Heaviside function on the interval [-1,1].
+% the delta function and the Heaviside function on the interval $[-1,1]$.
 % Note that we take the real part only to remove imaginary rounding errors.
 x = chebfun('x');
 hyperDelta = @(ep) real(F(x+1i*ep)-F(x-1i*ep));
@@ -50,20 +51,21 @@ hyperHeaviside = @(ep) real(G(x+1i*ep)-G(x-1i*ep));
 
 %%
 % We now imitate the limiting process by taking small values of the
-% parameter ep and plot the corresponding functions below:
+% parameter $\varepsilon$ and plotting the corresponding functions below:
+% First, delta functions.
+for ep = .1:-.01:.001;
+    plot(hyperDelta(ep)), hold on
+end
+title('Delta function','fontsize',12), hold off
 
 %%
-%
+% Next, Heaviside functions.
 for ep = .1:-.01:.001;
-    subplot( 1,2,1)
-    plot(hyperDelta(ep)), hold on
-    subplot( 1,2,2)
     plot(hyperHeaviside(ep)), hold on
 end
-subplot(1,2,1), title( 'Delta function' );
-subplot(1,2,2), title( 'Heaviside function' );
+title('Heaviside function','fontsize',12), hold off
 
 %% References
 %
-% 1. Urs Graf, Introduction to Hyperfunctions and Their Integral
-%    Transforms: An Applied and Computational Approach, Birkhaeuser, 2010.
+% 1. Urs Graf, _Introduction to Hyperfunctions and Their Integral
+%    Transforms: An Applied and Computational Approach_, Birkhaeuser, 2010.
