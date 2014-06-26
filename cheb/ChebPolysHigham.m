65c2a067b2db9b9ca6efa8ba41820afd5d10cae4
gradywright <gradywright@boisestate.edu>
2014-06-26 09:59:47 +0100

    Updated example to also reference Fornberg's book since a similar plot appears there. Included Legendre polynomials for comparison.

--- cheb/ChebPolysHigham.m
+++ cheb/ChebPolysHigham.m
@@ -1,14 +1,14 @@
-%% Chebyshev polynomials as plotted by Higham and Higham
+%% Chebyshev polynomials as plotted by Fornberg and Higham & Higham
 % Nick Trefethen, December 2011
 
 %%
 % (Chebfun example cheb/ChebPolysHigham.m)
-% [Tags: #Higham, #Chebyshevpolynomial, #PLOT3, #CHEBPOLY]
+% [Tags: #Higham, #Fornberg, #Chebyshevpolynomial, #PLOT3, #CHEBPOLY]
 
 %%
-% On p. 259 of their MATLAB Guide, 2nd ed. [1], Higham and Higham present an
-% attractive 3D plot of several Chebyshev polynomials. Here is their plot
-% reproduced in Chebfun.
+% Attractive 3D plots of several Chebyshev polynomails appear in both 
+% Fornberg's book [1] (p. 159) and Higham & Higham's book [2] (p. 259). 
+% Here are similar plots reproduced in Chebfun.
 
 k = [0 2 4 10 20 40 60];
 x = chebfun('x'); one = 1 + 0*x;
@@ -24,6 +24,22 @@ xlabel('k',FS,fs), ylabel('x',FS,fs), set(gca,FS,fs)
 h = get(gca,'xlabel'); set(h,'position',get(h,'position')+[1.5 0.1 0])
 h = get(gca,'ylabel'); set(h,'position',get(h,'position')+[0 0.25 0])
 
+% Fornberg also includes the Legendre polynomials for comparison. This can 
+% be easily done in Chebfun by replacing `chebpoly` with `legpoly` above.
+% Here is the result
+clf;
+for j = 1:length(k)
+  plot3(j*one,x,legpoly(k(j)),'linewidth',1.6), hold on
+end
+axis([1 length(k) -1 1 -1 1])
+box on
+set(gca,'dataaspectratio',[1 0.75 4]), view(-72,28)
+set(gca,'xticklabel',k)
+xlabel('k',FS,fs), ylabel('x',FS,fs), set(gca,FS,fs)
+h = get(gca,'xlabel'); set(h,'position',get(h,'position')+[1.5 0.1 0])
+h = get(gca,'ylabel'); set(h,'position',get(h,'position')+[0 0.25 0])
+
 %% References
 %
-% 1. D. J. Higham and N. J. Higham, _Matlab Guide_, 2nd ed., SIAM, 2005.
+% 1. B. Fornberg, _A Practical Guide to Pseudospectral Methods_, Cambridge University Press, 1996.
+% 2. D. J. Higham and N. J. Higham, _Matlab Guide_, 2nd ed., SIAM, 2005.
