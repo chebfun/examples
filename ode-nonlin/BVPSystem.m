dd6e15783184a031a603a59b7fbae328958b66fe
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-07-18 10:29:24 +0100

    polished ode-nonlin/BVPSystem

--- ode-nonlin/BVPSystem.m
+++ ode-nonlin/BVPSystem.m
@@ -9,13 +9,13 @@
 % Here is a system of two coupled nonlinear ODEs on the
 % interval $[-1,1]$, with boundary conditions.
 %
-% $$ u'' - sin(v) = 0, $$
+% $$ u'' - \sin(v) = 0, $$
 %
-% $$ v'' + cos(u) = 0, $$               
+% $$ v'' + \cos(u) = 0, $$               
 %
 % $$ u(-1) = 1,  ~~  v'(-1) = 0, ~~ u'(1) = 0,  ~~  v(1) = 0. $$
 
-%% Solution using multiple variables $u$ and $v$
+%% Solution using multiple variables `u` and `v`
 % One way you can solve a problem like this with Chebfun is to work with
 % multiple variables, solving for two chebfuns $u$ and $v$. Here we do this,
 % setting up the problem using anonymous functions that take two chebfuns as
@@ -36,21 +36,21 @@ LW = 'linewidth'; FS = 'fontsize';
 u = sol{1}; v = sol{2};
 figure, subplot(1,2,1), plot(u, LW, 2)
 hold on, plot(v,'--r', LW, 2), hold off
-title('u and v vs. x', FS, 12), legend('u', 'v')
+title('u and v vs. x', FS, 10), legend('u', 'v')
 box on, grid on
 xlabel('x', FS, 10), ylabel('u(x) and v(x)', FS, 10)
 subplot(1,2,2), semilogy(nrmduvec, '-*', LW, 2)
-title('Norm of update vs. iteration no.', FS, 12)
+title('Norm of update vs. iteration no.', FS, 10)
 box on, grid on
-xlabel('Iteration no.', FS, 10), ylabel('Norm of update', FS, 10)
+xlabel('iteration no.', FS, 10), ylabel('norm of update', FS, 10)
 
-%% Solution using a single indexed variable u
+%% Solution using a single indexed variable `u`
 % Another way to solve the same problem is to work with a single chebmatrix
-% variable u that has two components, `u{1}` and `u{2}`.
+% variable `u` that has two components, `u{1}` and `u{2}`.
 %
-% $$ (u_1)'' - sin(u_2) = 0, $$
+% $$ (u_1)'' - \sin(u_2) = 0, $$
 %
-% $$ (u_2)'' + cos(u_1) = 0, $$               
+% $$ (u_2)'' + \cos(u_1) = 0, $$               
 %
 % $$ u_1(-1) = 1, ~~ (u_2)'(-1) = 0, ~~ (u_1)'(1) = 0, ~~ u_2(1) = 0. $$
 %
@@ -72,10 +72,10 @@ nrmduvec = info.normDelta;
 clf
 subplot(1,2,1), plot(u{1}, LW, 2), hold on
 plot(u{2}, '--r', LW, 2), hold off
-title('u_1(x) and u_2(x) vs. x', FS, 12), legend('u_1', 'u_2')
+title('u_1(x) and u_2(x) vs. x', FS, 10), legend('u_1', 'u_2')
 box on, grid on
 xlabel('x', FS, 10), ylabel('u_1(x) and u_2(x)', FS, 10)
 subplot(1,2,2), semilogy(nrmduvec, '-*', LW, 2)
-title('Norm of update vs. iteration no.', FS, 12)
+title('Norm of update vs. iteration no.', FS, 10)
 box on, grid on
-xlabel('Iteration no.', FS, 10), ylabel('Norm of update', FS, 10)
+xlabel('iteration no.', FS, 10), ylabel('norm of update', FS, 10)
