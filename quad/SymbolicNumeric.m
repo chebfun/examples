5f621062bf9726933685932a73f17e7a8afa2381
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-07-26 16:16:45 +0100

    MathJaxifying the symbolic formula in quad/SymbolicNumeric

--- quad/SymbolicNumeric.m
+++ quad/SymbolicNumeric.m
@@ -13,87 +13,40 @@
 
 %%
 % For example, consider the function
-% 
+%
 % $$ f(x) = \log(2+x)^3 \log(3+x) x^3 $$
 %
 % The Online Integrator quickly delivers the following exact indefinite
 % integral:
 
 %%
-%
-% (-759744 - 558290 x + 17705 x  - 1050 x  + 54 x  + 
-%
-%   910528 Log[2 + x] + 400008 x Log[2 + x] - 
-%
-%          2                    3
-%   22836 x  Log[2 + x] + 2072 x  Log[2 + x] - 
-%
-%        4                               2
-%   162 x  Log[2 + x] - 302016 Log[2 + x]  - 
-%
-%                      2          2           2
-%   118800 x Log[2 + x]  + 11880 x  Log[2 + x]  - 
-%
-%         3           2        4           2
-%   1680 x  Log[2 + x]  + 216 x  Log[2 + x]  + 
-%
-%                   3                     3
-%   48384 Log[2 + x]  + 15552 x Log[2 + x]  - 
-%
-%         2           3        3           3
-%   2592 x  Log[2 + x]  + 576 x  Log[2 + x]  - 
-%
-%        4           3
-%   144 x  Log[2 + x]  + 309078 Log[3 + x] + 
-%
-%                              2
-%   79680 x Log[3 + x] - 5520 x  Log[3 + x] + 
-%
-%        3                  4
-%   592 x  Log[3 + x] - 54 x  Log[3 + x] - 
-%
-%   293976 Log[2 + x] Log[3 + x] - 
-%
-%   57600 x Log[2 + x] Log[3 + x] + 
-%
-%         2
-%   7488 x  Log[2 + x] Log[3 + x] - 
-%
-%         3
-%   1344 x  Log[2 + x] Log[3 + x] + 
-%
-%        4
-%   216 x  Log[2 + x] Log[3 + x] + 
-%
-%                    2
-%   138672 Log[2 + x]  Log[3 + x] + 
-%
-%                     2
-%   13824 x Log[2 + x]  Log[3 + x] - 
-%
-%         2           2
-%   3456 x  Log[2 + x]  Log[3 + x] + 
-%
-%         3           2
-%   1152 x  Log[2 + x]  Log[3 + x] - 
-%
-%        4           2
-%   432 x  Log[2 + x]  Log[3 + x] - 
-%
-%                   3
-%   46656 Log[2 + x]  Log[3 + x] + 
-%
-%        4           3
-%   576 x  Log[2 + x]  Log[3 + x] - 
-%
-%                                               2
-%   24 (5609 - 6756 Log[2 + x] + 4680 Log[2 + x] ) 
-%
-%    PolyLog[2, -2 - x] + 
-%
-%   288 (-563 + 780 Log[2 + x]) PolyLog[3, -2 - x] - 
-%
-%   224640 PolyLog[4, -2 - x]) / 2304
+% $$ (-759744 - 558290 x + 17705 x  - 1050 x  + 54 x  + \\\\
+%   910528 \text{Log}[2 + x] + 400008 x \text{Log}[2 + x] - \\\\
+%   22836 x^2  \text{Log}[2 + x] + 2072 x^3  \text{Log}[2 + x] - \\\\
+%   162 x^4  \text{Log}[2 + x] - 302016 \text{Log}[2 + x]^2  - \\\\
+%   118800 x \text{Log}[2 + x]^2  + 11880 x^2  \text{Log}[2 + x]^2  - \\\\
+%   1680 x^3  \text{Log}[2 + x]^2  + 216 x^4  \text{Log}[2 + x]^2  + \\\\
+%   48384 \text{Log}[2 + x]^3  + 15552 x \text{Log}[2 + x]^3  - \\\\
+%   2592 x^2  \text{Log}[2 + x]^3  + 576 x^3  \text{Log}[2 + x]^3  - \\\\
+%   144 x^4  \text{Log}[2 + x]^3  + 309078 \text{Log}[3 + x] + \\\\
+%   79680 x \text{Log}[3 + x] - 5520 x^2  \text{Log}[3 + x] + \\\\
+%   592 x^3  \text{Log}[3 + x] - 54 x^4  \text{Log}[3 + x] - \\\\
+%   293976 \text{Log}[2 + x] \text{Log}[3 + x] - \\\\
+%   57600 x \text{Log}[2 + x] \text{Log}[3 + x] + \\\\
+%   7488 x^2  \text{Log}[2 + x] \text{Log}[3 + x] - \\\\
+%   1344 x^3  \text{Log}[2 + x] \text{Log}[3 + x] + \\\\
+%   216 x^4  \text{Log}[2 + x] \text{Log}[3 + x] + \\\\
+%   138672 \text{Log}[2 + x]^2  \text{Log}[3 + x] + \\\\
+%   13824 x \text{Log}[2 + x]^2  \text{Log}[3 + x] - \\\\
+%   3456 x^2  \text{Log}[2 + x]^2  \text{Log}[3 + x] + \\\\
+%   1152 x^3  \text{Log}[2 + x]^2  \text{Log}[3 + x] - \\\\
+%   432 x^4  \text{Log}[2 + x]^2  \text{Log}[3 + x] - \\\\
+%   46656 \text{Log}[2 + x]^3  \text{Log}[3 + x] + \\\\
+%   576 x^4  \text{Log}[2 + x]^3  \text{Log}[3 + x] - \\\\
+%   24 (5609 - 6756 \text{Log}[2 + x] + 4680 \text{Log}[2 + x]^2 ) \\\\
+%    \text{PolyLog}[2, -2 - x] + \\\\
+%   288 (-563 + 780 \text{Log}[2 + x]) \text{PolyLog}[3, -2 - x] - \\\\
+%   224640 \text{PolyLog}[4, -2 - x]) / 2304 $$
 
 %%
 % In Chebfun, more prosaically, we could do this:
@@ -126,7 +79,7 @@ fi(1) - fi(-1)
 % that can arise in symbolic computing.  Of course, sometimes
 % no symbolic answer is available at all.
 % If $f$ is changed to
-% 
+%
 % $$ g(x) = \log(2+x)^3 \log(3+x)^2 x^3, $$
 %
 % then the Online Calculator reports: "Mathematica could not
@@ -140,8 +93,7 @@ title('not symbolically integrable',FS,14)
 
 fi = cumsum(f)
 
-%%
-% Reference:
-% 
+%% References
+%
 % 1.  L. N. Trefethen, Computing numerically with functions
 %     instead of numbers, _Communications of the ACM_, to appear.
