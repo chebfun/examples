8a03bee1c9a926d8add594a8fbb3d95a5a538ed5
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-07-28 20:50:46 +0100

    polished NullSpace

--- ode-eig/NullSpace.m
+++ ode-eig/NullSpace.m
@@ -3,32 +3,31 @@
 
 %%
 % (Chebfun example ode-eig/NullSpace.m)
-% [Tags: #linear, #eigenvalues, #optimization, #bvp, #nonstandard]
+% [Tags: #nullspace, #NULL, #SUBSPACE]
 
-LW = 'LineWidth'; lw = 2;
 
 %%
 % We've recently introduced some new functionality in Chebfun for computing the
 % nullspace of differential operators. Let's explore this with a couple of
-% simple examples!
+% simple examples.
 
-%% Simple Example #1
+%% 1. Simple example #1
 % Let's start as simply as we can, and take
-% $$ (Lu)(x) := u''(x), \qquad x~~\text{in}~~[-1, 1]. $$
-
+%
+% $$ (Lu)(x) := u''(x), \quad x\in [-1, 1]. $$
 L = chebop(@(u) diff(u, 2));
 
 %%
-% Clearly the nullspace --- that is the nontrivial functions $v$ for which
-% $ L(v) = 0 $ --- of this operator is spanned the two functions
-
+% Clearly the nullspace of this operator -- that is, the space
+% of functions $v$ for which
+% $L(v)=0$ -- is spanned the two functions
 v = [1, chebfun('x')];
 norm(L(v))
 
 %%
-% Supposing we didn't know this, we could compute the space with the `null()`
-% method.
-
+% Supposing we didn't know this, we could compute a basis
+% for the nullspace with the `null` method:
+LW = 'LineWidth'; lw = 1.6;
 V = null(L)
 plot(V, LW, lw)
 V'*V
@@ -45,27 +44,26 @@ norm(L(V))
 
 subspace(v, V)
 
-%% Incomplete boundary conditions
+%% 2. Incomplete boundary conditions
 % Now let's consider the more complicated 2nd-order operator
-% $$ Lu = u'' + 0.1x(1-x^2)u' + \sin(x)u \qquad (*) $$
-% for $x$ in $[-\pi, \pi]$.
-
+%
+% $$ Lu = u'' + 0.1x(1-x^2)u' + \sin(x)u, \quad
+% x\in [-\pi, \pi].  \qquad (*) $$
 dom = [-pi, pi];
 L = chebop(@(x, u) diff(u, 2) + .1*x.*(1-x.^2).*diff(u) + sin(x).*u, dom);
 
 %%
 % As before, it has a nullspace of rank 2.
-
 V = null(L)
 plot(V, LW, lw)
 V'*V
 norm(L(V))
 
 %%
-% However, now suppose we impose ONE boundary condition, say Dirichlet at the
-% left. This removes ONE degree of freedom, and we are now left with a rank 1
+% However, now suppose we impose one boundary condition, say,
+% a Dirichlet condition at the
+% left. This removes one degree of freedom, and we are left with a rank 1
 % nullspace.
-
 L.lbc = 0;
 L.rbc = [];
 v = null(L)
@@ -74,68 +72,63 @@ v'*v
 norm(L(v))
 
 %%
-% Clearly this null vector must satisfy the given condition $v(-pi) = 0.$
-
+% Clearly this null vector must satisfy the given condition $v(-\pi) = 0.$
 v(-pi)
 
-%% An application
-% Where might this be useful? Well, suppose we were interested in equation $(*)$
+%% 3. An application
+% Where might these ideas be useful?
+% Well, suppose we were interested in equation $(*)$
 % with a homogeneous Dirichlet condition at the left, and wanted to know what
 % inhomogeneous Dirichlet condition gave the minimal 2-norm of the solution to
 % $Lu = 1$.
-
-%%
 % Rather than solving the linear system for a number of different boundary
 % conditions (which would be computationally expensive) we could simply solve
-% for one, say again homogeneous Dirichlet
-
+% for one, say again a homogeneous Dirichlet condition,
 L.rbc = 0;
 u = L\1;
 hold on, plot(u, '--r', LW, lw), hold off
 
 %%
 % and compute the rest by adding a scalar multiple of the null-function $v$.
-
 E = chebfun(@(c) norm(u + c*v, 2), [-10, 10], 'vectorize', 'splitting', 'on');
-plot(E)
+plot(E,LW,lw)
 
 %%
 % We compute the 2-norm as a chebfun in the unknown variable $c$, which we can
 % then minimise to obtain the minimal energy solution
-
 [minE, c_star] = min(E)
 u_star = u + c_star*v
-plot(u_star)
+plot(u_star,LW,lw)
 
 %%
 % So the condition we require is that $u(\pi)$ = `bc_star`, where
 
 bc_star = u_star(pi)
-
-%% Exotic constraints
-% The `null` function can also handle the exotic types of boundary conditions
-% that can be enforced in Chebfun (see [1]). For example, suppose we wish to
-% again compute the null-space of the 3rd-order piecewise-smoooth ODE
-% $$ Lu := 0.1u''' + \sin(x)u'' + u, \quad x~~\text{in}~~[-1,1] $$
-% with the 'boundary' condition that
+%% 4. Exotic constraints
+% The Chebfun `null` function can also
+% handle the more exotic types of boundary conditions
+% that can be imposed in Chebfun (see [1]). For example, suppose we wish to
+% compute the nullspace of the 3rd-order piecewise-smoooth ODE
+%
+% $$ Lu := 0.1u''' + \sin(x)u'' + u, \quad x\in[-1,1] $$
+%
+% with the 'boundary' condition
+%
 % $$ \int(u) = u(0). $$
-
 dom = [-1, 1];
 L = chebop(@(x, u) .1*diff(u, 3) + sin(x).*diff(u, 2) + u, dom);
 L.bc = @(x, u) sum(u) - u(0);
 
 %%
 % Here `null` has no problems!
-
 V = null(L)
 plot(V, LW, lw), shg
 V'*V
 
 %%
-
 sum(V) - V(0,:)
 norm(L(V), 1)
 
-%% References
+%% 5. References
 %
 % 1. Chebfun Example [ode-linear/NonstandardBCs](../ode-linear/NonstandardBCs.html)
