28639ced4658e09dde3fb3ebe497c9d7a7cddcdf
Nick Hale <nick.p.hale@gmail.com>
2014-07-28 16:57:25 +0200

    Update NullSpace.m
    Updates for V5 and new implementation of `null()`.

--- ode-eig/NullSpace.m
+++ ode-eig/NullSpace.m
@@ -5,149 +5,133 @@
 % (Chebfun example ode-eig/NullSpace.m)
 % [Tags: #linear, #eigenvalues, #optimization, #bvp, #nonstandard]
 
-%%
-% We've recently introduced some new functionality in Chebfun for computing
-% the nullspace of differential operators. Let's explore this with a couple
-% of simple examples!
+LW = 'LineWidth'; lw = 2;
 
+%%
+% We've recently introduced some new functionality in Chebfun for computing the
+% nullspace of differential operators. Let's explore this with a couple of
+% simple examples!
 
 %% Simple Example #1
-% Let's start as simply as we can, and take
-%
-%     u''(x) = 0, for x in [-1 1]
+% Let's start as simply as we can, and take \[u''(x) = 0\], for $x$ in [-1 1].
 
-L = chebop(@(u) diff(u,2));
+L = chebop(@(u) diff(u, 2));
 
 %%
-% Clearly the nullspace, that is the nontrivial functions v for which
-%
-%     L(v) = 0,
-%
-% for this operator is spanned the two functions
+% Clearly the nullspace, that is the nontrivial functions v for which \[L(v) =
+% 0\], for this operator is spanned the two functions
 
-v = [1 chebfun('x')];
+v = [1, chebfun('x')];
 norm(L*v)
 
 %%
-% Supposing we didn't know this, we could compute the space with the new
-% `null` function.
+% Supposing we didn't know this, we could compute the space with the `null()`
+% method.
 
 V = null(L)
-plot(V)
+plot(V, LW, lw)
 V'*V
-norm(L*V)
+norm(L(V))
 
 %%
 % where we find that V'V = I and LV ~ 0 as required.
 
 %%
-% Clearly V doesn't correspond directly to 1 and x, since there is some
-% freedom in how we orthogonalise the basis. However, we can check that
-% V and {1,x} correspond to the same spaces by computing the angle
-% between the spaces with the `subspace` command.
+% Clearly V doesn't correspond directly to 1 and $x$, since there is some
+% freedom in how we orthogonalise the basis. However, we can check that V and
+% {1, $x$} correspond to the same spaces by computing the angle between the
+% spaces with the `subspace` command.
 
-subspace(v,V)
-
+subspace(v, V)
 
 %% Incomplete boundary conditions
-% Now let's consider the more complicated 2nd-order operator
-%
-%     Lu = u'' + .1*(1-x.^2)u' - sin(x)u, x in [-pi pi]                (*)
+% Now let's consider the more complicated 2nd-order operator \[Lu = u'' +
+% .1*(1-x.^2)u' - sin(x)u\], $x$ in $[-\pi \pi]$ (*).
 
-dom = [-pi pi];
-L = chebop(@(x,u) diff(u,2) + .1*x.*(1-x.^2).*diff(u) + sin(x).*u, dom);
-x = chebfun('x',dom);
+dom = [-pi, pi];
+L = chebop(@(x, u) diff(u, 2) + .1*x.*(1-x.^2).*diff(u) + sin(x).*u, dom);
 
 %%
 % As before, it has a nullspace of rank 2.
 
 V = null(L)
-plot(V)
+plot(V, LW, lw)
 V'*V
-norm(L(x,V(:,1)))
-norm(L(x,V(:,2)))
+norm(L(V))
 
 %%
-% However, now suppose we impose ONE boundary condition, say Dirichlet at
-% the left. This removes ONE degree of freedom, and we are now left with a
-% rank 1 nullspace.
+% However, now suppose we impose ONE boundary condition, say Dirichlet at the
+% left. This removes ONE degree of freedom, and we are now left with a rank 1
+% nullspace.
 
 L.lbc = 0;
 L.rbc = [];
 v = null(L)
-plot(v), shg
+plot(v, LW, lw), shg
 v'*v
-norm(L(x,v))
+norm(L(v))
 
 %%
-% Clearly this null vector must satisfy the given condition v(-pi) = 0.
+% Clearly this null vector must satisfy the given condition $v(-pi) = 0.$
 
+v(-pi)
 
 %% An application
-% Where might this be useful? Well, suppose we were interested in equation
-% (*) with a homogeneous Dirichlet condition at the left, and wanted to
-% know what inhomogeneous Dirichlet condition gave the minimal 2-norm of the
-% solution to Lu = 1.
+% Where might this be useful? Well, suppose we were interested in equation (*)
+% with a homogeneous Dirichlet condition at the left, and wanted to know what
+% inhomogeneous Dirichlet condition gave the minimal 2-norm of the solution to
+% $Lu = 1$.
 
 %%
 % Rather than solving the linear system for a number of different boundary
-% conditions (which would be computationally expensive) we could simply
-% solve for one, say again homogeneous Dirichlet
+% conditions (which would be computationally expensive) we could simply solve
+% for one, say again homogeneous Dirichlet
 
 L.rbc = 0;
 u = L\1;
-hold on, plot(u,'--r'), hold off;
+hold on, plot(u, '--r', LW, lw), hold off
 
 %%
-% and compute the rest by adding a scalar multiple of the null-function v.
+% and compute the rest by adding a scalar multiple of the null-function $v$.
 
-E = chebfun(@(c) norm(u+c*v,2),'vectorise',[-10 10],'splitting','on');
+E = chebfun(@(c) norm(u + c*v, 2), [-10, 10], 'vectorize', 'splitting', 'on');
 plot(E)
 
 %%
-% We compute the 2-norm as a chebfun in the unknown variable c,
-% which we can then minimise to obtain the minimal energy solution
+% We compute the 2-norm as a chebfun in the unknown variable c, which we can
+% then minimise to obtain the minimal energy solution
 
-[minE c_star] = min(E)
+[minE, c_star] = min(E)
 u_star = u + c_star*v
 plot(u_star)
 
 %%
-% So the condition we require is that u(pi) = bc_star, where
+% So the condition we require is that $u(\pi)$ = bc_star, where
 
 bc_star = u_star(pi)
 
 %% Exotic constraints
 % The `null` function can also handle the exotic types of boundary conditions
-% that can be enforced in Chebfun (see [1]). For example, suppose we wish
-% to again compute the null-space of the 3rd-order piecewise-smoooth ODE
-%
-%       Lu := 0.001u''' + sign(x)*u'' + u, x in [-1 1]
-%
-% with the 'boundary' condition that
-%
-%             sum(u) = u(0).
+% that can be enforced in Chebfun (see [1]). For example, suppose we wish to
+% again compute the null-space of the 3rd-order piecewise-smoooth ODE \[Lu :=
+% 0.1u''' + sin(x)*u'' + u, \] $x$ in $[-1 1]$, with the 'boundary' condition
+% that \[sum(u) = u(0).\]
 
-dom = [-1 1];
-L = chebop(@(x,u) 1e-2*diff(u,3) + sign(x).*diff(u,2) + u);
-L.lbc = []; L.rbc = [];
-L.bc = @(u) sum(u)-u(0);
-x = chebfun('x',dom);
+dom = [-1, 1];
+L = chebop(@(x, u) .1*diff(u, 3) + sin(x).*diff(u, 2) + u, dom);
+L.bc = @(x, u) sum(u) - u(0);
 
 %%
 % Here `null` has no problems!
 
 V = null(L)
-plot(V), shg
+plot(V, LW, lw), shg
 V'*V
 
 %%
 
-sum(V(:,1))-V(0,1)
-norm(L(x,V(:,1)),1)
-sum(V(:,2))-V(0,2)
-norm(L(x,V(:,2)),1)
-
+sum(V) - V(0,:)
+norm(L(V), 1)
 
 %% References
 %
