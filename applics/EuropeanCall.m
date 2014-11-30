aca1a7ea13a719b4812a9e6299bb1bef04b4c0c4
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-11-30 23:17:19 +0000

    Formatting hack for applics/EuropeanCall

--- applics/EuropeanCall.m
+++ applics/EuropeanCall.m
@@ -181,7 +181,7 @@ hold off, grid on
 % given by
 %
 % \begin{equation}
-%   g(y) = f(x(y))\ |dx/dy|.  \label{eq2}
+%   g(y) = f(x(y))\ \|dx/dy\|.  \label{eq2}
 % \end{equation}
 % 
 % The validity of this rule relies on some assumptions, the most relevant for
