24f7d44c949516a09281c35e0dce90ac2e027c54
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2015-12-10 11:51:38 +0000

    Updated cheb/Turbo now that 'turbo' is in Chebfun

--- cheb/Turbo.m
+++ cheb/Turbo.m
@@ -5,21 +5,22 @@
 % (Chebfun example cheb/Turbo.m)
 % [Tags: #Wang-Huybrechs, #contour integral, #Bernstein ellipse, #accuracy]
 
-function Turbo()
 %%
 % Chebfun works with standard floating-point arithmetic,
-% usually giving about 16 digits of relative accuracy.
+% usually giving about 15 digits of accuracy relative to the
+% overall scale of a function.
 % So what's going on in the following picture?
 f = chebfun(@exp);
-fturbo = chebfunturbo(@exp);
+fturbo = chebfun(@exp,'turbo');
 MS = 'markersize'; LW = 'linewidth';
 plotcoeffs(fturbo,'or',MS,8), grid on, hold on
 plotcoeffs(f,'.k',MS,14)
 title('Ordinary and turbocharged Cheb coeffs of exp(x)')
 
 %%
-% Somehow, using nothing but standard arithmetic, the |chebfunturbo|
-% code finds coefficients down to about twice the usual level
+% Somehow, using nothing but standard arithmetic, the |'turbo'|
+% flag leads to Chebfun finding
+% coefficients down to about twice the usual level
 % on a log scale.  And the improvement is genuine.  For
 % example, the tenth derivative of |f| is not accurate at all:
 f10 = diff(f,10); f10(0)
@@ -37,7 +38,7 @@ f10turbo = diff(fturbo,10); f10turbo(0)
 % Lyness and Moler showed the power of the idea for Taylor coefficients, using
 % circular contours [Lyness and Moler 1967],
 % and Fornberg showed that improved accuracy could
-% be achieved by an automatic determining of an effective
+% be achieved by an automatic determination of an effective
 % radius for the circle of integration [Fornberg 1981].
 % More recently,
 % Bornemann analyzed the effect of radius fully for Taylor coefficients,
@@ -50,14 +51,11 @@ f10turbo = diff(fturbo,10); f10turbo(0)
 % [Wang and Huybrechs 2014].
 
 %%
-% The code |chebfunturbo| listed at the end of this Example
-% implements a simple, nonoptimal version of this idea.
+% The |'turbo'| flag invokes a a simple, nonoptimal version of this idea.
 % We like to think of this as "turbocharged" since turbochargers
 % take the exhaust tail from an engine and extract some extra
-% power from it.
-
-%%
-% |chebfunturbo| starts from 
+% power from it.  The code is short, and you can see it
+% in the Chebfun file |@chebtech/constructorTurbo.m|.  It starts from 
 % an ordinary chebfun, calling its length $n$.
 % Then it computes $4n$ roots of unity and also the corresponding
 % points on a Bernstein ellipse of parameter $\rho_2 = \rho^{2/3}$,
@@ -84,13 +82,11 @@ semilogy(0:2*n-1,abs(chebcoeffs(fturbo)-cexact(1:2*n)),'.-r',LW,1,MS,3)
 title('Lines added to show accuracy'), hold off
 
 %%
-% Using methods like this, we expect to introduce a
-% |turbo| option in the chebfun constructor.  We don't
-% imagine that this will be relevant to the typical Chebfun
-% computation, but there may be some applications where it makes
-% a difference. 
-
-%%
+% A chebfun constructed with |turbo| is not more accurate
+% in an overall sense; it is merely that some of its higher-degree
+% coefficients may be more accurate.  Thus it is not all all the
+% case the |turbo| breaks the 16-digit barrier in a general way.
+% But there may be some applications where it makes a difference. 
 % As a small indication of enhanced accuracy, here is a chebfun
 % for the function $\exp(x) (1+100x^2)/(1+25x^2)$.  Mathematically,
 % the function has a root at $x= 0.1i$.  Evaluating the ordinary chebfun
@@ -102,31 +98,9 @@ f(0.1i)
 %%
 % The turbocharged version of $f$, on the other hand, gets full
 % accuracy.
-fturbo = chebfunturbo(ff);
+fturbo = chebfun(ff,'turbo');
 fturbo(0.1i)
 
-%%
-
-function fturbo = chebfunturbo(ff)  
-
-% Simple test code that returns a "turbocharged" chebfun for the
-% function @(x) ff(x) on [-1,1].  In many cases this will have
-% more Chebyshev coeffs than an ordinary Chebfun, and more accurate. 
-
-f = chebfun(ff);                         % ordinary chebfun made from ff
-n = length(f);                           % length of the ordinary 
-rho = eps^(-1/(n-1));                    % Chebfun's value of rho
-rho2 = rho^(2/3);                        % smaller rho defining a "turbo ellipse"
-n4 = 4*n;                                % no. of sample pts on turbo ellipse
-z = exp(2i*pi*(0:n4-1)'/n4);             % 4n roots of unity
-w = (rho2*z+1./(rho2*z))/2;              % corresponding points on turbo ellipse
-fc = fft(ff(w))/n4;
-c = 2*fc./rho2.^(0:n4-1)';
-c(1) = c(1)/2;                           % coeffs computed from turbo ellipse
-c = c(1:2*n);                            % keep only 2n of them
-if norm(imag(c))<1e-8, c = real(c); end  % discard imaginary rounding errors
-fturbo = chebfun(c,'coeffs');            % make the chebfun from its coeffs
-end
 
 %%
 % References:
@@ -149,7 +123,4 @@ end
 % 5. H. Wang and D Huybrechs, Fast and highly accurate computation
 % of Chebyshev expansions coefficients of analytic functions,
 % submitted, 2014.
-
-
-end
 
