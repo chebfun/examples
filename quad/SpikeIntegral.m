41062ad823664b6e14a4f6d68d384cdb9b277ae0
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2015-12-16 10:57:08 +0000

    various changes to SpikeIntegral

--- quad/SpikeIntegral.m
+++ quad/SpikeIntegral.m
@@ -1,5 +1,5 @@
 %% Spike integral
-% Nick Hale, October 2010
+% Nick Hale, October 2010, revised December 2015
 
 %%
 % (Chebfun example quad/SpikeIntegral.m)
@@ -13,10 +13,11 @@ f = @(x) sech(10*(x-0.2)).^2 + sech(100*(x-0.4)).^4 + ...
 % (which appears as F21F in [1]) over $[0, 1]$.
 
 %%
-% We create a Chebfun representation and plot the function.
-ff = chebfun(f,[0 1], 'splitting','on');
+% The Chebfun representation is a very high degree polynomial,
+% but this causes no difficulty.
+ff = chebfun(f,[0 1])
 LW = 'linewidth';
-plot(ff,'b',LW,1.6,'numpts',1e4), grid on
+plot(ff,'b',LW,1.6), grid on
 title('Spike function','FontSize',14)
 
 %%
@@ -28,11 +29,37 @@ title('Zoom, on semilogy axes','FontSize',14)
 % Now we compute the integral.  In order to estimate the time for this
 % computation, we create the chebfun again without plotting it.
 tic
-ff = chebfun(f,[0 1], 'splitting','on');
+ff = chebfun(f,[0 1]);
 sum(ff)
+toc
 
 %%
-% Time for creating this chebfun and integrating it:
+% Now the degree of that polynomial was forced to be extraordinarily
+% high in order to resolve the narrowest spike.  A much more
+% compressed representation of $f$ can be attained by constructing the chebfun
+% piecewise, using "splitting on".  As of December 2015,
+% if this is done with default parameters, Chebfun fails to detect
+% the narrowest spike:
+ff = chebfun(f,[0 1],'splitting','on')
+plot(ff,'b',LW,1.6), grid on
+title('Unresolved spike function with splitting on','FontSize',14)
+
+%%
+% We can fix the problem by forcing Chebfun to sample at
+% more points.  Note that the total number of parameters
+% is 25 times less than with the global representation.
+ff = chebfun(f,[0 1],'splitting','on','minSamples',100)
+plot(ff,'b',LW,1.6), grid on
+title('Resolved spike function with splitting on','FontSize',14)
+
+%%
+% If speed is all you care about, though, nothing has been gained over
+% the first, global approach.  We compute the chebfun again
+% and see that the integral is the same to full precision but
+% the timing is worse:
+tic
+ff = chebfun(f,[0 1],'splitting','on','minSamples',100);
+sum(ff)
 toc
 
 %% References
