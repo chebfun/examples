57b0bdd89c87ab46ad3d9ea12077b38667b7ae37
gradywright <gradywright@boisestate.edu>
2016-04-12 18:33:10 -0600

    Updates from LNT and the comments that arose from the Chebfun meeting on 6-April-2016.

--- temp/AtmosphericTemperature.m
+++ temp/AtmosphericTemperature.m
@@ -1,4 +1,4 @@
-%% Computing with an atmospheric dataset in spherefun
+%% Computing with an atmospheric dataset in Spherefun
 % Alex Townsend and Grady Wright, April 2016 
 
 %%
@@ -8,14 +8,14 @@
 %% 1. Introduction 
 % Spherefun is a part of Chebfun for computing with functions on the
 % sphere. The underlying approximation scheme is based on representing
-% function on the sphere by certain structure-preserving low rank
+% functions on the sphere by certain structure-preserving low rank
 % approximants [1]. Mathematically, most functions are of infinite rank on
 % the sphere with a notable exception being spherical harmonic functions.
 % However, many functions are numerically of low rank. When a function is
 % of low rank, Spherefun is surprisingly efficient.
 
-%% 2. Is atmospheric temperature of low rank? 
-% Recently, we were asked: Is the atmospheric temperature of the earth 
+%% 2. Is surface air temperature of low rank? 
+% Recently, we were asked: Is the surface air temperature of the earth 
 % of low rank? In this example, we apply Spherefun to one atmospheric 
 % dataset and find that it can be well approximated by a low rank
 % function.
@@ -26,7 +26,6 @@
 % obtained from the National Oceanic and Atmospheric Administration (NOAA)
 % Earth System Research Laboratory:
 load AtmosphericData;
-% load(websave('AtmosphericData.mat','http://math.boisestate.edu/~wright/AtmosphericData.mat'))
 
 %% 
 % We can use Spherefun to construct an approximation of $f$ and visualize 
@@ -46,9 +45,10 @@ f
 
 %% 
 % Spherefun calculated the rank of $f$ as 185.  Since the dataset is of
-% size 529x1024, we regard this as of low rank.  For a precise defintion,
-% see [2].
-
+% size 529x1024, this shows that the low rank representation is achieving
+% some useful compression of the orignal dataset, although the results are
+% not as dramatic as one often sees for smooth functions (see [2] for more
+% detailed discussions).
 
 %% 3. Investigating the atmospheric temperature
 % There are over a hundred commands in Spherefun and we can now 
@@ -64,7 +64,7 @@ f = f - 273.15;
 mean2( f )
 
 %% 
-% What is the temperature at the North and South pole? 
+% What is the temperature at the North and South poles? 
 
 f( 0, 0, 1 ) % North pole
 f( 0, 0, -1) % South pole 
@@ -86,7 +86,7 @@ contour( f, -40:5:40, 'LineWidth', 2 ), axis off, view([50 5]), hold on
 spherefun.plotEarth('k-'), hold off 
 
 %% 
-% What is the zonal mean temperatue?
+% What is the zonal mean temperature?
 
 zonalMean = mean(f,2);
 plot(zonalMean), xlim([0 pi])
@@ -94,7 +94,7 @@ xlabel('Co-latitude, \theta'), ylabel('Temperature (Celsius)')
 
 %% 4. Poisson solver 
 % We can also compute the steady heat profile with an external 
-% source of $f$, assuming there are no internal heat sinks or sources. 
+% source $f$, assuming there are no internal heat sinks or sources. 
 % This requires solving Poisson's equation on the sphere.
 
 [n, m] = length( f ); 
@@ -105,21 +105,42 @@ spherefun.plotEarth('k-'), title('Original dataset'), snapnow, hold off
 plot( steadyHeat ), colormap(jet), axis off, view([50 0]), hold on
 spherefun.plotEarth('k-'), title('Steady Heat'), snapnow, hold off
 
-%% 5. Gaussian filter
-% It is common to smooth data by applying a Gaussian filter.  Spherefun
-% allows you to do this with the filter command, which is the continuous 
-% analogue of the MATLAB command with the same name.  Here, is the result 
-% after applying a Gaussian filter: 
-
-g = smooth( f, 0.01 );
-surf(f), colormap(jet), axis off, view([50 0]), hold on
-spherefun.plotEarth('k-'), title('Original dataset'), snapnow, hold off
-surf(g), colormap(jet), axis off, view([50 0]), hold on
-spherefun.plotEarth('k-'), title('Filtered dataset'), snapnow, hold off
+%% 5. Scale-space selection using a Gaussian filter
+% It is common to smooth data by applying a Gaussian filter. This type of
+% filter also provides a means of analyzing data at various scales and
+% is particularly appealing as it does not introduce artificial structures
+% in the data, such as magnifying local extrema. 
 
 %%
-% This type of filter has been demonstrated to be particularly attractive
-% for smoothing global climate data [3].
+% In [3], this idea is applied to global climate data collected on the 
+% surface of the sphere, such as air surface temperature, to identify
+% features in the data that are robust over multiple scales. The |smooth|
+% command in Spherefun uses a Gaussian filter to smooth a function f. It 
+% has in optional parameter $\sigma$ that determines the length scale 
+% (as measured in radians at the equator of the unit sphere) at which
+% the smoothing occurs.
+
+%%
+% We can repeat the experiments of [3] easily on the surface air
+% temperature with this command.  In this paper, the authors analyze
+% smoothing at scales of 2, 10, and 20 degrees, which in radians are
+% correspond to $\sigma$ values
+sig = [2 10 20]*pi/180;
+
+%%
+% The smoothed temperature at these scales, together with original 
+% temperature are computed and plotted as
+
+surf(f), colormap(jet), axis off, view([50 0]), hold on
+spherefun.plotEarth('k-'), title('Original Temp.'), snapnow, hold off
+
+for j=1:3
+    fsmooth = smooth( f, sig(j) );
+    surf(fsmooth), colormap(jet), axis off, view([50 0]), hold on
+    spherefun.plotEarth('k-')
+    title(['Smoothed Temp., \sigma=' num2str(sig(j)*180/pi) ' degrees'])
+    snapnow, hold off
+end
 
 %% References 
 % [1] A. Townsend, H. Wilber, and G. B. Wright, Computing with functions in
