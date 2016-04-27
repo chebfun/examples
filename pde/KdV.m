14b66bc33107cd634a37aaa238b319f2ea787005
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2016-04-27 12:22:31 +0100

    updated KdV.m

--- pde/KdV.m
+++ pde/KdV.m
@@ -16,11 +16,11 @@
 % to each other, taking values $25$ and $23$.
 % We can set up for the calculation like this:
 A = 25; B = 23;
-dom = [0 20]; x = chebfun('x',dom);
-tspan = [0 .0156 .0156];
+dom = [0 6]; x = chebfun('x',dom);
+tspan = [0 .0156];
 S = spinop(dom,tspan);
 S.linearPart = @(u) - diff(u,3);
-S.nonlinearPart = @(u) -.5*diff(u.^2);  % why does u.*diff(u) fail?
+S.nonlinearPart = @(u) -.5*diff(u.^2); % spin cannot parse "u.*diff(u)"
 S.init = 3*A^2*sech(.5*A*(x-3)).^2 + 3*B^2*sech(.5*B*(x-4)).^2;
 
 %%
@@ -33,7 +33,7 @@ S.init = 3*A^2*sech(.5*A*(x-3)).^2 + 3*B^2*sech(.5*B*(x-4)).^2;
 % $t=0.0156$, it is as far ahead at was originally behind.
 pause off
 tic, u = spin(S,spinpref('plot','off')); t = toc;
-plot(u)
+plot(S.init), hold on, plot(u), hold off
 
 %%
 % When `spin` is run is this simple mode without extra arguments,
@@ -48,7 +48,7 @@ t
 % time step, like this.  Here we do this and see that the image
 % looks the same:
 tic, u = spin(S,spinpref('plot','off','N',800,'dt',.00001)); t = toc;
-plot(u)
+plot(S.init), hold on, plot(u), hold off
 
 %%
 % However, the computation is much faster:
@@ -62,7 +62,7 @@ t
 % First of all, here is the taller of the two solitons just as before.
 S.init = 3*A^2*sech(.5*A*(x-3)).^2;
 u = spin(S,spinpref('plot','off','N',800,'dt',.00001));
-plot(u)
+plot(S.init), hold on, plot(u), hold off
 
 %%
 % What if we make the initial pulse a bit wider?  This is no
@@ -71,21 +71,24 @@ plot(u)
 % not in the form of solitons.
 S.init = 3*A^2*sech(.35*A*(x-3)).^2;
 u = spin(S,spinpref('plot','off','N',800,'dt',.00001));
-plot(u)
+plot(S.init), hold on, plot(u), hold off
 
 %%
 % If we make the pulse still wider, we get a beautiful train
 % of solitons.
 S.init = 3*A^2*sech(.05*A*(x-3)).^2;
 u = spin(S,spinpref('plot','off','N',800,'dt',.00001));
-plot(u)
+plot(S.init), hold on, plot(u), hold off
 
 %%
 % Let's try something a little bit random:
 S.init = 500*(x-12).*exp(-(x-12).^2);
 u = spin(S,spinpref('plot','off','N',800,'dt',.00001));
-plot(u)
+plot(S.init), hold on, plot(u), hold off
 
 %% 3. References
 % The mathematics of solitons is thoroughly understood.  See
-% for example [references needed].
+% for example [1].
+%
+% [1] M. J. Ablowitz and H. Segur, _Solitons and the
+% Inverse Scattering Transform_, SIAM, 1981.
