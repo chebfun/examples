35afb322a69ad9db27f876977b41404eb50e367b
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2016-05-09 17:32:26 +0100

    minor changes

--- sphere/Gravity.m
+++ sphere/Gravity.m
@@ -6,28 +6,29 @@
 % [Tags: #spherefun]
 
 %% 1. A little geometry
-% Here's a vector $X$ whose 2-norm is 1.5:
+% Here's a vector $X$ with 2-norm 1.5, which we will think of
+% as a spacecraft in orbit around the unit sphere:
 X = [-1  -1.1, -.2];
 norm(X)
 
 %%
 % Let's use the vector-valued part of
 % Spherefun to define the field of vector distances
-% between points on the unit sphere and $X$, which we can
-% think of as a spacecraft in orbit:
+% between $X$ and points on the sphere and $X$:
 d = spherefunv(@(x,y,z) X(1)-x, @(x,y,z) X(2)-y, @(x,y,z) X(3)-z);
 
 %%
-% Here is the scalar function representing the _distance_ $r$ between
-% $X$ and points on the sphere:
+% Here is the scalar function representing $|d|$, that is, the
+% the scalar distance between $X$ and points on the sphere:
 r = sqrt(d(1).^2 + d(2).^2 + d(3).^2); 
 
 %%
-% The minimum distance of $X$ to the sphere is of course $0.5%:
+% Here we confirm that the closest point on the sphere to $X$ is at
+% distance $0.5$:
 min_distance = min2(r)
 
 %%
-% Similarly, the maximum is $2.5$
+% Similarly, the farthest point is at distance $2.5$:
 max_distance = max2(r)
 
 %% 
@@ -35,21 +36,19 @@ max_distance = max2(r)
 % together with a red dot showing our little spacecraft.
 contour(r,.6:.1:2,'k')
 hold on, plot3(X(1),X(2),X(3),'.r','markersize',25), hold off
-view(-10,35)
+view(-10,35), axis equal, axis off
 
-%% 2. Inverse-square gravitational force
+%% 2. Inverse-square force
 % A great discovery of Newton (or was it Hooke?) is that the gravitational
 % forces associated with a sphere of uniform mass distribution are
 % the same as if all the mass were concentrated at the center.
 % Accordingly, we know that if a unit mass is spread around the
 % sphere and the spacecraft also has unit mass, then the 
-% inverse-square attraction between them should be $(1.5)^{-2}$,
-% since the distance from the spacecraft to the center of the sphere is $1.5%:
+% inverse-square attraction between them should be $(1.5)^{-2}$:
 force_exact = 1/1.5^2
 
 %%
-% Let's confirm Newton's observation by computing the net gravitational
-% force as an integral over the surface of the sphere.
+% Let's confirm this prediction by computing the integral over the sphere.
 % Since the area of the sphere is $4\pi$, the density of a uniformly
 % distributed mass is
 rho = 1/(4*pi)
@@ -61,5 +60,5 @@ Xnormalized = X/norm(X);
 force_function = rho*(Xnormalized*d)./r.^3;
 
 %%
-% Summing, we get the right answer:
+% Summing, we get the expected answer:
 force = sum2(force_function)
