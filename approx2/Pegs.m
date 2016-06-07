654c2ecdcbb37e3f451192cad48ab0b0eac65725
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2016-06-07 17:14:03 +0100

    new example approx2/Pegs

---
+++ approx2/Pegs.m
@@ -0,0 +1,46 @@
+%% Low-rank compression of Square and round pegs
+% Nick Trefethen, June 2016
+
+%%
+% (Chebfun example approx2/Pegs.m)
+
+%%
+% A recent example called "Low-rank approximation and
+% alignment with axes" explored the phenomenon that
+% the alignment of a function $f(x,y)$ with the $x$ and $y$
+% axes may greatly affect the rank of its representation
+% in Chebfun2.  Here we look at some more examples from
+% `cheb.gallery2`.
+
+%%
+% Here's the "tilted peg" example from the gallery.
+[f,fa] = cheb.gallery2('tiltedpeg'); fa
+
+%%
+% A plot shows this this is a somewhat smoothed version of
+% the characteristic function of a tilted square:
+contourf(f,.1:.1:.9), axis equal
+set(gca,'xtick',-1:1,'ytick',-1:1)
+FS = 'fontsize';
+text(-.9,.8,['rank ' int2str(rank(f))],FS,18)
+
+%%
+% If the peg is aligned with the axes, it is a separable
+% function, of rank 1:
+[f,fa] = cheb.gallery2('squarepeg'); fa
+contourf(f,.1:.1:.9), axis equal
+set(gca,'xtick',-1:1,'ytick',-1:1)
+text(-.9,.8,['rank ' int2str(rank(f))],FS,18)
+
+%%
+% A round peg has an in-between rank:
+[f,fa] = cheb.gallery2('roundpeg'); fa
+contourf(f,.1:.1:.9), axis equal
+set(gca,'xtick',-1:1,'ytick',-1:1)
+text(-.9,.8,['rank ' int2str(rank(f))],FS,18)
+
+%%
+% There are analogous examples in the Diskfun gallery
+% collection to be released before long.  There, the
+% round peg has rank 1 whereas the square peg does not.
+% Tilting does not matter.
