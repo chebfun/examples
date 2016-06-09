86da1f96e012f3d87c22e5533b01e030af4de3c6
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2016-06-09 10:28:39 +0100

    modifications to TwoElectrons

--- ode-nonlin/TwoElectrons.m
+++ ode-nonlin/TwoElectrons.m
@@ -17,9 +17,9 @@
 % just  a very special configuration with $n=2$, in which
 % the two electrons are assumed to be exactly symmetrical about a line
 % of reflection.  Here is a typical trajectory over a time interval
-% of length 50.  
+% of length 40.  
 LW = 'linewidth'; MS = 'markersize'; lw = 1.2;
-N = chebop(0,50);
+N = chebop(0,40);
 N.op = @(t,z) diff(z,2) + 2*z./abs(z).^3 - 0.25i*imag(z)./imag(z).^3;
 N.lbc = [1i; 1];
 tic, z = N\0; x = real(z); y = imag(z);
@@ -50,12 +50,12 @@ plot(x), xlabel t, ylabel('x(t)'), ylim([-1.5 1.5])
 
 %% 2. Energy
 % The kinetic energy of this motion is $|z'|^2$, and the
-% potential energy is $-4/|z| + 1/\hbox{Im} z$.  Thus the total energy is
-% $$ E = |z'|^2 -{4\over |z|} + {1\over \hbox{Im} z}, $$
-% and this quantity will be conserved.  For our initial value
-% $V=1$, the energy is $E = -2$:
-Energy = abs(diff(z)).^2 - 4./abs(z) + 1./imag(z);
-plot(Energy), xlabel t, ylabel('Energy(t)')
+% potential energy is $-4/|z| + 1/2\hbox{Im} z$.  Thus the total energy is
+% $$ E = |z'|^2 -{4\over |z|} + {1\over 2\hbox{Im} z}, $$
+% and this quantity is conserved.  For our initial value
+% $V=1$, the energy is $E = -2.5$.  (We do not verify
+% this by a Chebfun computation, which would be very slow because
+% of the near-singularities when $\hbox{Im}z$ is near zero.)
 
 %%
 % Note that a particle at $z=\infty$ with zero velocity
@@ -67,7 +67,7 @@ plot(Energy), xlabel t, ylabel('Energy(t)')
 % interesting to explore "Pluto" trajectories just below this limit
 % starting from values such as $V=1.85$ or $1.86$.
 
-%% 3. Periodic solutions
+%% 3. Periodic trajectories
 % For certain special initial velocities, the orbits are periodic.
 % The simplest one, corresponding to $V\approx 1.446$, has
 % the electrons simply swinging back and forth:
@@ -79,40 +79,33 @@ plot(x,y,x,-y,LW,lw), axis(1.2*[-1 1 -1 1]), axis square, hold off
 
 %%
 % A plot of $x$ values confirms the periodicity:
-plot(x)
+plot(x), xlabel t, ylabel x
 
 %%
 % Here is an estimate of the period:
-r = max(x,'local')
-diff(r)
+r = roots(x-.9*max(x)); r = r(deriv(x,r)>0); Period = r(2)-r(1)
 
 %%
-% Some special values:
+% Here is another periodic solution, with an estimate of the period:
 N.lbc = [1i; 0.783];
 z = N\0; x = real(z); y = imag(z);
 plot(0,0,'.k',MS,8), hold on
 plot(x,y,x,-y,LW,lw), axis(1.2*[-1 1 -1 1]), axis square, hold off
+r = roots(x-.9*max(x)); r = r(deriv(x,r)>0); Period = r(2)-r(1)
 
 %%
-plot(x)
-r = max(x,'local')
-diff(r)
-
-%%
-% Some special values:
+% And here is another:
 N.lbc = [1i; 1.17745];
 z = N\0; x = real(z); y = imag(z);
 plot(0,0,'.k',MS,8), hold on
 plot(x,y,x,-y,LW,lw), axis(1.2*[-1 1 -1 1]), axis square, hold off
-toc
+r = roots(y-.99999); Period = mean(r(end-1:end))
 
 %%
-plot(x)
-r = max(x,'local')
-diff(r)
+% Readers trying these computations on their own may enjoy
+% experimenting with the command |comet(z)|.
 
-%%
-% 4. Computing time
+%% 4. Computing time
 % Chebfun is wonderfully convenient, but it is not especially
 % fast, as we can see from the computing time for this
 % example:
