840db5f468510ab82e73b84642dee183e7468803
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-03-03 17:17:21 +0000

    a few changes

--- ode-eig/LevelRepulsionODE.m
+++ ode-eig/LevelRepulsionODE.m
@@ -13,8 +13,8 @@
 
 %%
 % To do this, we're going to use fourth-order differential
-% operators, not second-order.  The reason is that second
-% order self-adjoint operators put us in the territory of
+% operators, not second-order.  The reason is that second-order
+% self-adjoint operators put us in the territory of
 % Sturm-Liouville theory, where repeated eigenvalues are
 % impossible.  This is analogous to the situation for
 % real symmetric tridiagonal matrices with nonzero subdiagonal
@@ -26,7 +26,7 @@
 % If $L(t)$ is a real self-adjoint fourth-order linear
 % differential operator depending on a parameter $t$, then for each
 % $t$ there will be 
-% infinitely many real eigenvalues. As $t$ varies, the 
+% infinitely many real eigenvalues. As $t$ varies,
 % the eigenvalues will vary continuously.
 
 %%
@@ -44,27 +44,28 @@ L = @(a) chebop(@(x,u) diff(u,4)+a*diff(u,2)+exp(x/20)*u,[-5,5], ...
      @(u) [u;diff(u)],@(u) [u;diff(u)]);
 
 %%
-% We would now like to get our hands on the $6$ functions of $t$ representing
-% the first 6 (whatever that means) eigenvalues of $L(t)$.  In Chebfun, a convenient format for this
-% result will be a quasimatrix with $6$ columns.  
+% We would now like to get our hands on the six functions of $t$ representing
+% the first six (whatever that means) eigenvalues of $L(t)$.
+% In Chebfun, a convenient format for this
+% result will be a quasimatrix with six columns.  
 
 %%
 % We can construct this quasimatrix as follows.  (The `splitting off` command
 % has no effect, since splitting off is the default, but is included to show
 % where one would put `splitting on` to handle a problem with curves actually
 % crossing or coming very close.)
-ek = @(e,k) e(k);            % returns kth element of the vector e
+ek = @(e,k) e(k);                % returns kth element of the vector e
 eigL = @(t) sort(eigs(L(t)));    % returns sorted eigenvalues of the matrix A
 d = [4.4 5.4];
 E = chebfun; tic
 for k = 1:6
-    disp(k)
    E(:,k) = chebfun(@(t) ek(eigL(t),k),d,'splitting','off','eps',1e-4,'vectorize');
 end
 FS = 'fontsize'; LW = 'linewidth'; MS = 'markersize';
 figure, plot(E,LW,1.6), grid on
 title('Eigenvalues of L(t)',FS,16);
 xlabel('t',FS,12)
+ylim([-6 2])
 
 %%
 % One sees that two curves nearly cross, but not quite.  This brings us
@@ -75,3 +76,7 @@ xlabel('t',FS,12)
 % bit variable, we have broken that symmetry.  Changing 20 to 40 would
 % make the coefficient function closer to constant, and the near-crossing
 % would be even closer.
+
+%%
+% Alas, this took a while:
+time_in_seconds = toc
