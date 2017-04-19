02fd9eb21b3725ddd5742c90db3f96a7aa4521df
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-04-19 11:54:35 +0100

    various changes to Random2D

--- approx2/Random2D.m
+++ approx2/Random2D.m
@@ -1,5 +1,5 @@
 %% Random functions in 2D
-% Nick Trefethen, April 2015
+% Nick Trefethen, April 2017
 
 %%
 % (Chebfun example approx2/Random2D.m)
@@ -8,37 +8,39 @@
 %%
 % Recently Chebfun added the command |randnfun| for generating
 % smooth random
-% functions in 1D as well as the periodic |randfuntrig|.
+% functions in 1D, as well as its periodic cousin |randfuntrig|.
 % In keeping with Chebfun's mission of realizing continuous
-% analogues of the discrete objects of Matlab, |randnfun| can
-% be regarded as a continuous analogue of |randn|.
+% analogues of the familiar discrete objects, |randnfun| can
+% be regarded as a continuous analogue of the Matlab command |randn|.
 % Chebfun can construct 2D random functions too, with
 % |randnfun2| and |randnfun2trig|.  Random functions
 % in 3D or on the sphere or the disk have not yet been implemented.
 
 %%
-% A technical paper has not yete been written describing these
-% functions, but in a word, the idea is that a ``smooth random function''
+% A technical paper has not yet been written describing these
+% functions, but in a word, the idea is that a "smooth random function"
 % is constructed from a finite Fourier series with independent
-% normally distributed random coefficients. A parameter |dt| must
-% be specified that specifies the associated space scale.  Approximately
+% normally distributed random coefficients. A parameter dt must
+% be specified that sets the associated space scale.  Approximately
 % speaking, a random function contains wave numbers up to about
-% $2\pi/dt$..
+% $2\pi/dt$.
 
 %%
-% To illustrate, here is a random function with $dt = 0.2$ on
+% To illustrate, here is a random function with $dt = 0.4$ on
 % a $2\times 1$ rectangle.  Negative values are black and
 % positive values are white.
-dt = 0.1;
+dt = 0.2;
 rng(0), f = randnfun2(dt, [0 2 0 1]);
 plot(f), view(0,90), colormap(gray(2))
 axis equal, axis([0 2 0 1])
+LW = 'linewidth'; XT = 'xtick'; YT = 'ytick';
+set(gca,XT,0:.5:2,YT,0:.5:1)
 
 %%
 % A contour plot shows more:
-LW = 'linewidth';
-contour(f,LW,2.5)
+contour(f,LW,2.5), colormap('default'), colorbar
 axis equal, axis([0 2 0 1])
+set(gca,XT,0:.5:2,YT,0:.5:1)
 
 %%
 % To isolate the zero contours to high accuracy (though it takes
@@ -46,21 +48,30 @@ axis equal, axis([0 2 0 1])
 c = roots(f);
 plot(c,LW,3)
 axis equal, axis([0 2 0 1])
-axis equal, axis([0 2 0 1])
+set(gca,XT,0:.5:2,YT,0:.5:1)
 
 %%
-% Here's a 3D plot
+% Here's a 3D plot.
 plot(f)
+view(-20,50), camlight left
 
 %% 
-% Here for comparison is a periodic random function:
+% Here for comparison is a periodic random function.
 f = randnfun2trig(dt, [0 2 0 1]);
 plot(f), view(0,90), colormap(gray(2))
 axis equal, axis([0 2 0 1])
+set(gca,XT,0:.5:2,YT,0:.5:1)
 
 %%
-% And here is a random function with $dt = 0.05$.
-dt = 0.05;
-f = randnfun2(dt, [0 2 0 1]);
+% And here are random functions with $dt = 0.1$
+dt = 0.1; f = randnfun2(dt, [0 2 0 1]);
 plot(f), view(0,90), colormap(gray(2))
 axis equal, axis([0 2 0 1])
+set(gca,XT,0:.5:2,YT,0:.5:1)
+
+%%
+% and with $dt = 0.05$
+dt = 0.05; f = randnfun2(dt, [0 2 0 1]);
+plot(f), view(0,90), colormap(gray(2))
+axis equal, axis([0 2 0 1])
+set(gca,XT,0:.5:2,YT,0:.5:1)
