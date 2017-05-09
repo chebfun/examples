6ea150e7113d736bb3c5104f65f75d9e02a898ff
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-05-09 16:40:12 +0100

    tweaks

--- approx2/Zebra.m
+++ approx2/Zebra.m
@@ -5,32 +5,31 @@
 % (Chebfun example approx2/Zebra.m)
 
 %%
-% Sometimes less is more.  Instead of a plot showing
+% Instead of a plot showing
 % many function values, we may prefer to show just a
 % plus/minus distinction.  For this there is the
 % |'zebra'| option in Chebfun2, Spherefun, and Diskfun.
 
 %%
 % For example, here is zebra plot of a certain function on
-% the disk.  Negative values are black and positive values are white.
+% the disk.
+% For fun we've changed the colors from the usual black/white.
 cheb.xydisk;
 f = sin(20*(x+y).*(1+y));
 plot(f, 'zebra')
+colormap([1 1 0; 0 0 0])
 axis off
 
 %%
+% Normally, however, the plots show zebras rather than bumblebees.
+% Negative values are black and positive values are white.
 % Here is an example on the sphere.
 f = spherefun.sphharm(15,5);
 plot(f,'zebra')
 axis off
 
 %%
-% And here is a rectangle.
+% And here is an example on the unit square.
 f = randnfun2(.1);
 plot(f, 'zebra')
 axis equal off
-
-%%
-% Zebras, tigers, and bumblebees can be treated
-% with adjustments of the colormap.
-colormap([1 1 0; 0 0 0])
