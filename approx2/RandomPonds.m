bbf3c7b91375bbff0b5fb6ca16ac2e8865738a86
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-05-10 09:26:09 +0100

    inew example RandomPonds

---
+++ approx2/RandomPonds.m
@@ -0,0 +1,45 @@
+%% Random ponds in a 2D landscape
+% Nick Trefethen, May 2017
+
+%%
+% (Chebfun example approx2/RandomPonds.m)
+
+%%
+% Suppose $f$ is a 2D random function defining a "random landscape",
+% which is filled with water up to a level $h$.  The water collects
+% into random ponds, as we may illustrate with this figure for $h=-1$.
+blueblack = [.6 .6 1; 0 0 0];
+FS = 'fontsize'; CO = 'color';
+h = -1;
+f = randnfun2(.2);
+plot(f-h, 'zebra'), axis equal off, colormap(blueblack)
+title(['h = ' num2str(h)],FS,20,CO,'w')
+
+%%
+% Of course, the mathematics depends on one's notion of a random function,
+% and Chebfun makes a particular choice defined by certain random Fourier
+% coefficients.
+
+%%
+% If $h$ is lower, the ponds are smaller and more separated.
+h = -2;
+plot(f-h, 'zebra'), axis equal off, colormap(blueblack)
+title(['h = ' num2str(h)],FS,20,CO,'w')
+
+%%
+% As $h$ gets bigger, the ponds grow and connect into a giant
+% body of water.  This is related to the subject of percolation theory.
+for h = 0:2
+   plot(f-h, 'zebra'), axis equal off, colormap(blueblack)
+   title(['h = ' num2str(h)],FS,20,CO,'w')
+   snapnow
+end
+
+%%
+% We can also let $h$ vary across the domain.  The resulting image is
+% reminiscent of an engraving by Escher.
+dom = [-3 3 -1 1];
+f = randnfun2(.1, dom);
+h = chebfun2(@(x,y) x, dom);
+plot(f-h, 'zebra'), axis equal off, colormap(blueblack)
+
