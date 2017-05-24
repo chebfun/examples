3fb4648a9d02d1e6fb98da2410df540bc1dcd587
Silviu Filip <filips@maths.ox.ac.uk>
2017-05-24 09:57:22 +0100

    some small changes

--- approx/RationalAbsx.m
+++ approx/RationalAbsx.m
@@ -1,5 +1,5 @@
 %% Rational approximation of abs(x) with minimax
-% Sivliu Filip, Yuji Nakatsukasa, and Nick Trefethen, May 2011
+% Silviu Filip, Yuji Nakatsukasa, and Nick Trefethen, May 2017
 
 %%
 % (Chebfun example approx/RationalAbsx.m)
@@ -18,20 +18,20 @@
 % A solution to this problem is to represent $r$
 % in barycentric form instead, $r = N/D$, where $N$ and $D$ are
 % partial fractions based on certain adaptively selected support points.
-% This idea led to Chebfun |aaa| algorithm a few months ago [2], and now
+% This idea led to the Chebfun |aaa| algorithm a few months ago [2], and now
 % it has further led to an improvement in our capabilities
-% for rational best approximation on an interval.  The old |remez| has
-% been replaced by a new and much more powerful |minimax| [1].
+% for rational best approximation on an interval.  The old |remez| code has
+% been replaced by a new and much more powerful |minimax| command [1].
 % We believe this is the most powerful implementation of the Remez
-% algorithm ever produced. 
+% algorithm ever produced.
 
 %%
 % As a famous example in this area, let us consider type $(n,n)$ rational
 % approximation of $|x|$ on $[-1,1]$ for various $n$.
-% Up to 2016, Chebfun's |remez| code was only able to go up to 
+% Up to 2016, Chebfun's |remez| code was only able to go up to
 % $(8,8)$.  For example, on p. 192 of [3], a plot is presented of
 % errors up to type $(50,50)$, but they are taken from a table rather
-% than computed on the fly. 
+% than computed on the fly.
 
 %%
 % Now, by contrast, here we go to type $(80,80)$.  Chebfun has to work
