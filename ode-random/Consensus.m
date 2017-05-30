291dd884999b1fa34e196c9a5ea1a43dc1965267
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2017-05-30 21:56:56 +0100

    tweaks

--- ode-random/Consensus.m
+++ ode-random/Consensus.m
@@ -17,8 +17,10 @@ f = randnfun(lambda,dom,'norm');
 g = randnfun(lambda,dom,'norm');
 N.op = @(t,u,v) [diff(u) + f; diff(v) + g];
 d = 1; N.lbc = @(u,v) [u-d; v+d];
-LW = 'linewidth'; lw = 2;
+LW = 'linewidth'; lw = 2.5; FS = 'fontsize';
 [u,v] = N\0; plot(u,LW,lw), grid on, hold on, plot(v,LW,lw), hold off
+xlabel('t',FS,32), ylabel('u,v',FS,32)
+title('Two independent random walks',FS,32)
 
 %%
 % Now let's change things by making the particles attract each
@@ -28,6 +30,8 @@ F = 3;
 N.op = @(t,u,v) [diff(u) + f + F*(u-v).*exp(-(u-v).^2); ...
                  diff(v) + g + F*(v-u).*exp(-(v-u).^2)];
 [u,v] = N\0; plot(u,LW,lw), grid on, hold on, plot(v,LW,lw), hold off
+xlabel('t',FS,32), ylabel('u,v',FS,32)
+title('Walks strongly attracted together',FS,32)
 
 %%
 % Like Richard Burton and Elizabeth Taylor, the
@@ -38,9 +42,11 @@ F = 1.0;
 N.op = @(t,u,v) [diff(u) + f + F*(u-v).*exp(-(u-v).^2); ...
                  diff(v) + g + F*(v-u).*exp(-(v-u).^2)];
 [u,v] = N\0; plot(u,LW,lw), grid on, hold on, plot(v,LW,lw), hold off
+xlabel('t',FS,32), ylabel('u,v',FS,32)
+title('Walks weakly attracted together',FS,32)
 
 %%
-% This model has connection with work by Eitan Tadmor and others on
+% This model has connections with work by Eitan Tadmor and others on
 % ``social hydrodynamics''.  See for example [1].
 
 %%
