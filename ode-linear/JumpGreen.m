d5b8dd7fadfd5960f1de88534de6d6a646442ab0
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2019-06-21 18:25:22 +0100

    improvements to JumpGreen

--- ode-linear/JumpGreen.m
+++ ode-linear/JumpGreen.m
@@ -15,11 +15,11 @@ L.lbc = 0; L.rbc = 0;
 
 %%
 % But suppose we want a continuous solution that whose derivative jumps
-% by $1/\eta$ at $x=1/2$?  Mathematically, this is like having one 2nd-order BVP on 
+% by $-1/\eta$ at $x=1/2$?  Mathematically, this is like having one 2nd-order BVP on 
 % $[0,1/2]$ coupled to another on $[1/2,1]$, and we will need four
 % boundary conditions in total.  The two additional boundary conditions
-% will assert that the derivative jumps by $-1/\eta$ whereas the function
-% value does not jump at $x=1/2$.  Chebfun allows you to specify
+% will assert that at $x=1/2$, the derivative jumps by $-1/\eta$ whereas the function
+% value is continuous.  Chebfun allows you to specify
 % these conditions like this:
 L.bc = @(x,u) [jump(u,1/2) ; jump(diff(u),1/2)+eta];
 plot(L\0), grid on
@@ -27,14 +27,14 @@ plot(L\0), grid on
 %%
 % Incidentally, |jump| is an abbreviation based on
 % a more general Chebfun capability involving evaluations on the 
-% left and the right of a point.  For example, we could do
-% something like this:
+% left and the right of a point.  For example, we could do this:
 L.bc = @(x,u) [feval(u,.7,'left')-2 ; feval(u,.7,'right')-1];
 plot(L\0), grid on
 
 %%
-% Or if we a jump in the function value and continuity of
-% the derivative, we could do this:
+% Returning to the convenience of |jump|, suppose we want
+% a jump in the function value and continuity of
+% the derivative.  We could do this:
 L.bc = @(x,u) [jump(u,.2)-1; jump(diff(u),.2)];
 plot(L\0), grid on
 
