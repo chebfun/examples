b3780867a37e341db12ef18845357c48578306e2
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2019-07-09 13:52:49 +0100

    adding arcl length computation

--- approx2/Belyaev.m
+++ approx2/Belyaev.m
@@ -17,25 +17,40 @@ wave = @(k) real(a(1)*exp(i*pi*(k*x-y)) + a(2)*exp(i*pi*(k*x+y)) ...
 r = roots(wave(8));
 plot(r, LW, 2)
 axis([-1 1 -1 1]), axis square, set(gca,XT,[],YT,[])
-title(['number of components: ' int2str(size(r,2))],FS,10)
+title(['number of components: ' int2str(size(r,2))],FS,12)
 
 %%
 % The Chebfun2 |roots| command has picked out the distinct
-% components of the zero set in the unit square.  These computations
-% are delicate, however, and Chebfun does not always get this number
-% right (nor does it always get the curves accurately).
-% Here we set $k=16$:
+% components of the zero set in the unit square: it is a quasimatrix
+% with 23 columns:
+size(r)
+
+%%
+% Here are the arc lengths of the pieces, sorted from smallest to largest:
+arclength = @(f) norm(diff(f),1);
+np = size(r,2);
+al = zeros(np,1);
+for k = 1:np
+    al(k) = arclength(r(:,k));
+end
+sort(al)
+
+%%
+% Computations with |roots| in Chebfun2 are delicate, and the number
+% of components does not always come out right; nor are the curves
+% always accurate.  Here we seem to be doing well, though.
+% We repeat the computation with $k=16$:
 r = roots(wave(16));
 plot(r, LW, 1.2)
 axis([-1 1 -1 1]), axis square, set(gca,XT,[],YT,[])
-title(['number of components: ' int2str(size(r,2))],FS,10)
+title(['number of components: ' int2str(size(r,2))],FS,12)
 
 %%
-% And here we set $k=32$:
+% And with $k=32$:
 r = roots(wave(32));
 plot(r, LW, .7)
 axis([-1 1 -1 1]), axis square, set(gca,XT,[],YT,[])
-title(['number of components: ' int2str(size(r,2))],FS,10)
+title(['number of components: ' int2str(size(r,2))],FS,12)
 
 %%
 % Total time for this example:
