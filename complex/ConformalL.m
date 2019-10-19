1eece7ed186c53ce05321e25119d823d18b02a77
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2019-10-19 12:48:10 +0100

    new ConformalL example

---
+++ complex/ConformalL.m
@@ -0,0 +1,102 @@
+%% Conformal mapping of an L-shaped region
+% Nick Trefethen, October 2019
+
+%%
+% (Chebfun example complex/ConformalL.m)
+% [Tags: #Conformal]
+
+%%
+% The problem of conformally mapping a simply connected domain
+% $\Omega$ to the unit disk, with $f(z_c) = 0$ for
+% a given point $z_c\in \Omega$, can be
+% reduced to a Laplace problem on $\Omega$.  If $u$ is a real harmonic function that
+% satisfies $u(z) = -\log|z-z_c|$ on the boundary of $\Omega$,
+% then the conformal map is $f(z) = (z-z_c)\exp (u(z) + i v(z))$, where
+% $v$ is the harmonic conjugate of $u$.  To solve this Laplace
+% problem numerically, an easy method is to expand $u$ as a linear combination of a
+% suitable family of harmonic functions -- real and imaginary
+% parts of analytic functions -- and find expansion coefficients by
+% least-squares fitting in a set of sample points on the boundary [2].
+
+%%
+% This process gives the ``boundary correspondence function'', the
+% homeomorphism between the boundary of $\Omega$ and the unit circle.
+% From here, one can then approximate both
+% $f$ and its inverse by rational functions using
+% AAA approximation as described in [1], invoking the
+% the Chebfun |aaa| command.  The resulting representations of
+% $f$ and $f^{-1}$ can typically be evaluated in microseconds
+% per point.
+
+%%
+% Here we apply this idea to a simple example of 
+% "Schwarz--Christoffel mapping without the
+% Schwarz--Christoffel formula".  The following commands
+% determine the map of an L-shaped domain.  We make
+% use of an expansion basis consisting of two kinds of terms:
+% powers $(z-z_0)^k$ for a point $z_0$ interior to $\Omega$,
+% and fractional powers $z^{2k/3}$ to handle the singularity at
+% the reentrant corner at $z=0$.
+  tic, N = 24; 
+  Z = 1i*(1-tanh(12*linspace(1,0,5*N)'));         % sample pts on bndry
+  Z = [Z(1:end-1); chebpts(2*N,[1i -1+1i])];
+  Z = [Z(1:end-1); chebpts(2*N,[-1+1i -1-1i])]; 
+  a = exp(.25i*pi);                               % 45 degree rotation
+  Z = a*[Z/a; conj(Z(end-1:-1:1)/a)];
+  mZ = a^3*Z/norm(Z,inf);                         % avoid branch cut
+  z0 = -.5-.5i; cZ = (Z-z0)/norm(Z-z0,inf);
+  k1 = (0:N); k2 = (1:N);                         % exponents for (z-z0)^k terms
+  m1 = k1*(2/3); m2 = k2*(2/3);
+  m1(3:3:end) = []; m2(3:3:end) = [];             % exponents for z^(2k/3) terms
+  A = [real(cZ.^k1) imag(cZ.^k2) ...              % least-squares matrix
+       real(mZ.^m2) imag(mZ.^m2)];
+  zc = -.2-.2i;
+  U = -log(abs(Z-zc));                            % right-hand side
+  c = A\U;                                        % solve the problem
+  boundary_err = norm(A*c-U,inf)                  % measure residual
+  V = [imag(cZ.^k1) -real(cZ.^k2) ...             % conjugate harmonic fun
+       imag(mZ.^m2) -real(mZ.^m2)]*c;
+  W = (Z-zc).*exp(U+1i*V); W = W/W(1);            % boundary correspondence
+  [f,pol] = aaa(W,Z,'tol',10*boundary_err);       % conformal map
+  [finv,polinv] = aaa(Z,W,'tol',10*err);          % inverse map
+
+%%
+% Here is a plot of the map.  The red dots show the poles
+% of the AAA approximation, which represents $f$ to about
+% six digits of accuracy.
+  LW = 'linewidth'; MS = 'markersize';
+  plot(Z,'b',LW,1)
+  axis(1.5*[-1 1 -1 1]), axis equal, hold on, axis off
+  plot(pol,'.r',MS,8)
+  ray = chebpts(100); ray = ray(ray>=0);
+  for th = 2*pi*(1:12)/12, plot(finv(exp(1i*th)*ray),'k'), end
+  circ = exp(2i*pi*(0:200)/200);
+  for r = .1:.1:.9, plot(finv(r*circ),'k'), end, hold off
+  number_of_poles_of_f = length(pol)
+
+%% 
+% The curves just plotted are the images under $f^{-1}$ of
+% circles and radial lines in the unit disk, which we
+% now plot.  The red dots are the poles of $f^{-1}$.
+  plot(W,'b',LW,1)
+  axis(1.5*[-1 1 -1 1]), axis equal, hold on, axis off
+  plot(polinv,'.r',MS,8)
+  for th = 2*pi*(1:12)/12, plot(exp(1i*th)*ray,'k'), end
+  for r = .1:.1:.9, plot(r*circ,'k'), end, hold off
+  number_of_poles_of_finv = length(polinv)
+
+%%
+total_time_for_this_example = toc
+
+%%
+% [1] A. Gopal and L. N. Trefethen,
+% Representation of conformal maps by rational functions,
+% _Numer. Math._ 142 (2019), 359-382.
+%
+% [2] L. N. Trefethen,
+% Conformal mapping in Chebfun, Chebfun example
+% |chebfun.org/examples/complex/ConformalMapping.html|.
+%
+% [3] L. N. Trefethen,
+% Numerical conformal mapping with rational functions,
+% manuscript in preparation, 2019.
