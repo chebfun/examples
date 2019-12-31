34fb606526d865061c0563cc1bf1e54f214225cc
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2019-12-31 09:59:06 +0000

    new section in Hosepipe.m

--- approx2/Hosepipe.m
+++ approx2/Hosepipe.m
@@ -4,7 +4,7 @@
 %%
 % (Chebfun example approx2/Hosepipe.m)
 
-%%
+%% 1. Hosepipe
 % Here is a surface you might find on your vacuum
 % cleaner or under the hood of your car:
 r = chebfun(@(x) .5+.04*cos(40*x));
@@ -40,6 +40,29 @@ plotcoeffs(G)
 % One reason for representing a periodic function periodically
 % is that it is somewhat more efficient.  More important
 % in practice is that its derivatives may retain their
-% smoothness and accuracy at the wraparound point.  Perhaps
-% a reader of this example will have an idea of how to
-% extend it to illustrate the advantages of |'trigy'|?
+% smoothness and accuracy at the wraparound point.
+
+%% 2. Annulus
+% The hosepipe example is relatively complicated.  A simpler illustration
+% of the Cheb/trig combination is the discretization of a function 
+% defined in an annulus: periodic in the angular direction, nonperiodic
+% in the radial direction.  For example, the analytic function
+% $f(z) = (1+4/z^3)^{-1} (z^3+0.1)^{-1}$ has three poles outside the unit
+% disk and three inside.  For $1/2 \le |z| \le 3/2$ it is analytic,
+% however, so its abolute value $F(r,t) = |f(re^{i t})|$ is smooth:
+f = @(z) (1+4./z.^3).^-1.*(z.^3+.1).^-1;
+F = @(r,t) abs(f(r.*exp(1i*t)));
+
+%%
+% Here we make a cheb/trig chebfun2 of $F$:
+Fc = chebfun2(@(r,t) F(r,t),[.5 1.5 -pi pi],'trigy');
+
+%%
+clf, plot(Fc), colorbar
+xlabel r, ylabel t
+
+%%
+% (I am not sure if Chebfun offers an elegant way to plot this
+% in an annulus.)  Here are the Chebyshev and Fourier expansion
+% coefficients:
+plotcoeffs(Fc)
