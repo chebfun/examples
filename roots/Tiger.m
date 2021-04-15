8f289d46b053d0bf0598e6760ff2b644ee5f4ec0
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2021-04-15 11:07:52 +0100

    minor updates to Tiger

--- roots/Tiger.m
+++ roots/Tiger.m
@@ -11,34 +11,33 @@
 % with an example that reminds one of a tiger's tail.
 % Here with a few modifications is that example:
 x = chebfun('x',[-2 1]);
-LW = 'linewidth'; MS = 'markersize';
 CO = 'color'; orange = [1 .5 .25];
 f = 2*exp(.5*x).*(sin(5*x) + sin(101*x));
 roundf = round(f);
 r = roots(f-roundf,'nojump');
-hold off, plot(f,LW,2,CO,orange), hold on
+hold off, plot(f,CO,orange), hold on
 ylim([-8 6])
-plot(r,f(r),'.k',MS,12), hold off
+plot(r,f(r),'.k'), hold off
 
 %%
 % Let's look at what's going on here.  First of all a
 % chebfun $f$ is constructed:
-plot(f,LW,1.6,CO,orange)
+plot(f,CO,orange)
 ylim([-8 6])
 
 %%
 % Then another chebfun is constructed consisting of $f$ rounded
 % to integers:
-plot(roundf,LW,0.8,'-k','jumpline','k')
+plot(roundf,'k','jumpline','k')
 ylim([-8 6])
 
 %%
 % Superimposing the two curves yields a lot of intersections,
 % which are computed by `roots`:
 number_of_roots = length(r)
-plot(f,LW,1.6,CO,orange), hold on
-plot(roundf,LW,0.8,'-k','jumpline','k')
-plot(r,f(r),'.k',MS,8), hold off
+plot(f,CO,orange), hold on
+plot(roundf,'k','jumpline','k')
+plot(r,f(r),'.k'), hold off
 
 %%
 % In [1], dots appear not only where $f$ is equal to
@@ -49,4 +48,4 @@ plot(r,f(r),'.k',MS,8), hold off
 %% Reference
 %
 % 1. L. N. Trefethen, _Approximation Theory and Approximation
-%    Practice_, SIAM, 2013.
+%    Practice, Extended Edition_, SIAM, 2019.
