4ed6098a4301c063cb62200cb05795c0a5e4be0b
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2022-01-14 17:38:03 +0000

    updated Checkmark.m

--- approx/Checkmark.m
+++ approx/Checkmark.m
@@ -7,10 +7,10 @@
 
 %%
 % A paper has appeared as arXiv:2102.09502v1 by P. D. Dragnev, A. R. Legg, and R. Orive, 
-% called "On the best uniform polynomial approximation to the checkmark function".
+% called "On the best uniform polynomial approximation to the checkmark function."
 % The problem considered in this paper is degree $n$ best
 % polynomial approximation of the function
-% $f(x) = |x-\alpha|$ on $[-1,1]$.  They ask, how
+% $f(x) = |x-\alpha|$ on $[-1,1]$.  The authors ask, how
 % does the error $E_n(\alpha)$ depend on $\alpha$?
 
 %%
@@ -21,7 +21,7 @@ x = chebfun('x');
 f = @(a) abs(x-a);
 p = @(a,n) minimax(f(a),n);
 e = @(a,n) norm(p(a,n)-f(a),inf);
-chebfuneps 1e-3, splitting on
+chebfuneps 1e-6, splitting on
 E = [];
 for n = 1:7
    en = chebfun(@(a) e(a,n),[0,1]);
@@ -35,7 +35,7 @@ chebfuneps factory, splitting off
 red = [.9 0 0]; blue = [0 0 .9];
 ax = axes; ax.ColorOrder = [red; blue]; hold on
 plot(E(:,2:3)), grid on
-xlabel('$\alpha$','interpreter','latex'), ylabel('$E_n(\alpha)$','interpreter','latex')
+xlabel('\alpha'), ylabel('E_n(\alpha)')
 title('n = 2 and 3') 
 %%
 % As a numerical check, let us look at the 
@@ -51,7 +51,7 @@ title('n = 2 and 3')
 close, ax = axes; ax.ColorOrder = [blue; red]; hold on
 plot(E), grid on
 xlabel('\alpha'), ylabel('E_n(\alpha)')
-title('n = 1,2,...,7'), ylim([0 .6]), hold off
+title('n = 1,2,...,7'), ylim([0 .5]), hold off
 
 %%
 % Unfortunately, although all this is very compact and natural
